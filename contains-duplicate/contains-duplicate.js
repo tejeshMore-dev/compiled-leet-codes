b30eb3d389fa1f59895a7e71fc6165389628cc5a
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2021-12-26 20:02:35 +0530

    Time: 84 ms (74.33%), Space: 48.5 MB (10.02%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let set = new Set();
+    
+    for( let num of nums ) {
+        if( set.has(num) ) return true
+        
+        set.add( num );
+    }
+    
+    return false
+};
