32d619e63191d5887a5ff417e9331a8d9fe27698
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-09 09:01:34 +0530

    Time: 112 ms (24.18%), Space: 39.3 MB (85.43%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    let result = 0;
+    let result = Number.MAX_VALUE;
     
     let p1 = 0;
     let p2 = 0;
@@ -13,7 +13,7 @@ var minSubArrayLen = function(target, nums) {
     while ( p1 !== nums.length && p2 !== nums.length ) {
         if( sum + nums[p2] >= target ) {
             sum -= nums[p1];
-            result = result === 0 ? p2 - p1 + 1 : Math.min( result, p2 - p1 + 1 );
+            result = Math.min( result, p2 - p1 + 1 );
             p1++;
         } else {
             sum += nums[p2];
@@ -22,5 +22,5 @@ var minSubArrayLen = function(target, nums) {
         
     }
     
-    return result;
+    return result === Number.MAX_VALUE ? 0 : result;
 };
