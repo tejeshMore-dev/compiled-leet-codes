e0b5788a426a1d55a846b3ad7faf334b5561f4ed
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-14 02:12:54 +0530

    Time: 116 ms (16.49%), Space: 38.6 MB (88.09%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let lp = 0, rp = nums.length-1;
+    
+    while( lp < rp ) {
+        let mid = Math.floor( lp + (rp-lp)/2 );
+        
+        if( nums[mid] > nums[rp] )
+            lp = mid + 1;
+        else
+            rp = mid
+    }
+    
+    return nums[lp];
+};
