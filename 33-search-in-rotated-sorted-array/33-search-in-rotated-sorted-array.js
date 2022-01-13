4b092a8e346c69ffbd2cece8beff86f87987b7c1
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-14 04:01:10 +0530

    Time: 115 ms (16.65%), Space: 40.3 MB (9.08%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let lp = 0, rp = nums.length-1;
+    
+    while ( lp < rp ) {
+        let mid = Math.floor( lp + (rp-lp)/2 );
+        console.log(mid)
+        if( nums[mid] === target ) return lp = mid;
+        
+        if( target > nums[mid] ) {
+            if( target > nums[rp] && nums[mid] < nums[lp] ) {
+                rp = mid-1;
+            } else {
+                lp = mid+1;
+            }
+        } else {
+            if( target < nums[lp] && nums[mid] > nums[rp] ) {
+                lp = mid+1;
+            } else {
+                rp = mid-1;
+            }
+        }
+    }
+    
+    return nums[lp] === target ? lp : -1;
+};
