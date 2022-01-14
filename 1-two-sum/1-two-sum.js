ff86c3cdbfc17d334f75d9bb53fd0b85766c805f
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-14 21:08:26 +0530

    Time: 129 ms (38.12%), Space: 41 MB (25.12%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    
+    for( let i=0; i<nums.length; i++ ) {
+        if( map.has(nums[i]) ) {
+            return [ map.get(nums[i]), i ];
+        }
+        
+        map.set(target-nums[i], i);
+    }
+    
+};
