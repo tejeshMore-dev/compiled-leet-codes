7f2146ab201dc603d4e3b0485194233d40f0f273
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-16 03:30:10 +0530

    Time: 202 ms (18.92%), Space: 46 MB (30.04%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let lp = 0, hp = nums.length-1, pointer = nums.length-1;
+    let result = new Array(nums.length);
+    
+    while( lp <= hp ) {
+        if( Math.abs(nums[lp]) > Math.abs(nums[hp]) ) {
+            result[pointer] = Math.pow(nums[lp], 2);
+            lp++;
+            pointer--;
+        } else {
+            result[pointer] = Math.pow(nums[hp], 2);
+            hp--;
+            pointer--;
+        }
+    }
+    
+    return result;
+};
