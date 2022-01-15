c7e6513305a23217f82eef33b00bbf95782e540f
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-16 05:03:01 +0530

    Time: 212 ms (9.20%), Space: 50.9 MB (97.33%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.js
+++ 896-monotonic-array/896-monotonic-array.js
@@ -8,8 +8,8 @@ var isMonotonic = function(nums) {
     let inc = true, dec = true;
     
     for( let i=0; i<nums.length -1; i++ ) {
-        if( nums[i] > nums[i+1] ) inc = false
-        if( nums[i] < nums[i+1] ) dec = false
+        if( nums[i] > nums[i+1] ) dec = false
+        if( nums[i] < nums[i+1] ) inc = false
         
     }
     
