23238a937ff444fffe1dd23d5cddebc8dc25fc6e
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-16 02:15:06 +0530

    Time: 115 ms (16.25%), Space: 38.9 MB (51.18%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -6,7 +6,6 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
     while( n > 0 ) {
         if(  nums1[m-1] > nums2[n-1]  ) {
             nums1[m+n-1] = nums1[m-1];
