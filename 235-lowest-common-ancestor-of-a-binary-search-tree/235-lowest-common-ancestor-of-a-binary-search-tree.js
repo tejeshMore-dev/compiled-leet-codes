6a22c7209ce4b0991f2fa3177e930bc29585bbcd
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-17 05:32:11 +0530

    Time: 245 ms (5.03%), Space: 51.2 MB (8.70%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,29 +13,14 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-//     if( root.val === p.val || root.val === q.val || ( Math.min(p.val, q.val) < root.val && Math.max(p.val, q.val) > root.val ) ) {
-//         console.log(root)
-//         return root
+    if( root.val === p.val || root.val === q.val || ( Math.min(p.val, q.val) < root.val && Math.max(p.val, q.val) > root.val ) ) {
+        console.log(root)
+        return root
         
-//     }
-
-//     if( root.val > p.val && root.val > q.val )
-//         lowestCommonAncestor(root.left, p, q)
-//     else if( root.val < p.val && root.val < q.val )
-//         lowestCommonAncestor(root.right, p, q)
-    
-//     return root
-    
-    	// condition 1: if both p and q node value if less than the root node value,
-	// that means that both nodes lie on its left side
-    if(root.val > p.val && root.val > q.val) {
-        return lowestCommonAncestor(root.left, p, q);
-    } else if(root.val < p.val && root.val < q.val) {
-		// condition2: if both p and q node value is greater than the root node value, 
-		// that means that both nodes lie on its right side
-        return lowestCommonAncestor(root.right, p, q);
     }
-	// condition 3: if p and q node lie on either side of the root node, that means
-	// root node itself is the lowest common ancestor, so return root node
-    return root;
+
+    if( root.val > p.val )
+        return lowestCommonAncestor(root.left, p, q)
+    else 
+        return lowestCommonAncestor(root.right, p, q)
 };
