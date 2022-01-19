5f49a2c0ed50792b6a19acd1c045ca8d441ee354
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-20 01:02:22 +0530

    Time: 172 ms (5.39%), Space: 40.8 MB (55.39%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.js
+++ 402-remove-k-digits/402-remove-k-digits.js
@@ -3,47 +3,25 @@
  * @param {number} k
  * @return {string}
  */
-// var removeKdigits = function(num, k) {
-//     let stack = [], i = 0;
-
-// 	while (i < num.length) {
-//         if(k>0 && stack.length > 0 && stack[stack.length-1] > num[i]) {
-//             stack.pop();
-//             k--;
-//         }
-
-//         stack.push(num[i]);
-//         i++;
-//     }
-//     while(k--) {
-//         stack.pop();
-//     }
-
-//     while( stack[0] === '0' && stack.length > 1 ) {
-//         stack.shift();
-//     }
-    
-//     return stack.join('')
-
-// };
-
 var removeKdigits = function(num, k) {
-    if(num.length===k)return '0';
-    
-    const stack=[];
-    
-    for(let i=0;i<num.length;i++){
-        while(k&&stack.length>0&&stack[stack.length-1]>num[i]){
-            stack.pop()
-            k--
+    let stack = [], i = 0;
+
+	while (i < num.length) {
+        while(k>0 && stack.length > 0 && stack[stack.length-1] > num[i]) {
+            stack.pop();
+            k--;
         }
-        stack.push(num[i])
+
+        stack.push(num[i]);
+        i++;
     }
-    while(k--){
-        stack.pop()
+    while(k--) {
+        stack.pop();
     }
-    while(stack[0]==="0"&&stack.length>1){
-        stack.shift()
+
+    while( stack[0] === '0' && stack.length > 1 ) {
+        stack.shift();
     }
-    return stack.join('')
+    
+    return stack.length > 0 ? stack.join('') : '0';
 };
