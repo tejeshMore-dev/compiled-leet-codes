e5c26c635689a0edd02924c97abbcc231c5958d0
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-21 02:10:30 +0530

    Time: 88 ms (44.38%), Space: 40.7 MB (5.08%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,25 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let lp = 0, rp = nums.length-1;
+    let lp = 0, rp = nums.length-1, result = -1;
     
-    while ( lp < rp ) {
+    while( lp < rp ) {
         let mid = Math.floor( lp + (rp-lp)/2 );
-        if( nums[mid] === target ) return lp = mid;
+        console.log(mid)
+        if( target === nums[mid] ) return lp = mid
         
         if( target > nums[mid] ) {
-            if( target > nums[rp] && nums[mid] < nums[lp] ) {
-                rp = mid-1;
+            if(  target > nums[rp] && nums[mid] < nums[lp] ) {
+                rp = mid - 1;
             } else {
-                lp = mid+1;
+                lp = mid + 1; 
             }
         } else {
             if( target < nums[lp] && nums[mid] > nums[rp] ) {
-                lp = mid+1;
+                lp = mid + 1
             } else {
-                rp = mid-1;
+                rp = mid - 1
             }
         }
+        
+        
     }
     
     return nums[lp] === target ? lp : -1;
