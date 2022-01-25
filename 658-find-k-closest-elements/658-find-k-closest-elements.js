c93ebbe5798ec47467746a427e651f5ae2278eab
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-25 21:57:03 +0530

    Time: 544 ms (5.11%), Space: 57.3 MB (5.10%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+    let lp = 0, rp = arr.length-1, ans = [];
+    
+    while( rp - lp + 1 > k ) {
+        let left = Math.abs( arr[lp] - x );
+        let right = Math.abs( arr[rp] - x )
+        console.log(left, right)
+        if( left <= right )
+            rp--;
+        else
+            lp++;
+    }
+    
+    for( let i = 0; i < k; i++ ) {
+        ans.push(arr[lp+i]);
+    }
+    
+    return ans;
+};
