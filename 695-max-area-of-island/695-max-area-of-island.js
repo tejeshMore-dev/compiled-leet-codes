935230d53e13c67023223334ed1d2ceb31116be6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-25 16:38:22 +0530

    Time: 225 ms (7.86%), Space: 46.8 MB (8.61%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -0,0 +1,79 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let maxArea = 0, rows = grid.length, columns = grid[0].length;
+    let directions = [ [0, -1], [0, 1], [1,0], [-1,0] ] // left right top bottom
+    let queue = [];
+    let visited = new Set();
+    
+    for( let r=0; r<rows; r++ ) {
+        for( let c=0; c<columns; c++ ) {
+            if( grid[r][c] && !visited.has(`${r}_${c}`) ) {
+                maxArea = Math.max(findArea([[r,c]]), maxArea);
+            }
+        }
+    }
+    
+    function findArea(queue) {
+        let area = 0;
+        while( queue.length > 0 ) {
+            let [r,c] = queue.shift();
+            area++;
+            visited.add(`${r}_${c}`);
+            directions.map((dir) => {
+               let [ row, col ] = [ r+dir[0], c+dir[1] ];
+                
+                if( row < 0 || col < 0 || row >= rows || col >= columns || grid[row][col] === 0 || visited.has(`${row}_${col}`) ) {
+                } else {
+                    visited.add(`${row}_${col}`);
+                    queue.push([row, col])       
+                }
+            });
+            // console.log(area, queue)
+            
+        }
+        return area;
+    }
+    
+    return maxArea;
+};
+
+
+// var maxAreaOfIsland = function(grid) {
+//     let ans = 0, n = grid.length, m = grid[0].length
+//     const trav = (i, j) => {
+//         if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0
+//         grid[i][j] = 0
+//         return 1 + trav(i-1, j) + trav(i, j-1) + trav(i+1, j) + trav(i, j+1)
+//     }
+//     for (let i = 0; i < n; i++) 
+//         for (let j = 0; j < m; j++)
+//             if (grid[i][j]) ans = Math.max(ans, trav(i, j))
+//     return ans
+// };
+
+
+/*
+max =
+    
+    max =  0;
+    direction = [];
+    rows 
+    columns
+    
+    iterate row
+        col
+        
+        if(i)
+        find area
+        update max
+        
+        
+        find area
+        bfs 
+        mark counted ones
+        sum 
+
+*/
