597af18dee7006aa320469a43fddb50ee7998598
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-27 17:50:52 +0530

    Time: 145 ms (29.15%), Space: 52.5 MB (7.99%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,9 +13,9 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    
     if( p.val < root.val && q.val < root.val )
         return lowestCommonAncestor(root.left, p, q);
+    
     if( p.val > root.val && q.val > root.val )
         return lowestCommonAncestor(root.right, p, q);
     
