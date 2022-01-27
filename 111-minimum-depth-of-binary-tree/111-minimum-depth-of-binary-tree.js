d27a6b7338f4e36abb7bb7ede87f0a7a13341e22
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-27 13:50:11 +0530

    Time: 444 ms (10.39%), Space: 98.8 MB (47.72%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -11,14 +11,14 @@
  * @return {number}
  */
 var minDepth = function(root) {
-    if(!root)
+    if( !root )
         return 0
-    if( !root.left && root.right  )
-        return minDepth(root.right) + 1;
-
-    if( root.left && !root.right  )
-        return minDepth(root.left) + 1;
-
-    return Math.min( minDepth(root.left), minDepth(root.right) ) + 1;
-
+    
+    if( !root.left )
+        return minDepth(root.right) + 1
+        
+    if( !root.right )
+        return minDepth(root.left) + 1
+    
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1
 };
