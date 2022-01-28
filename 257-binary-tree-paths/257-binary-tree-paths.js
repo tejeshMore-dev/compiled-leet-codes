216f9a87214d299092627e6c64a8554ae1eb6c8f
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-28 19:42:18 +0530

    Time: 112 ms (33.89%), Space: 43.2 MB (5.03%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.js
+++ 257-binary-tree-paths/257-binary-tree-paths.js
@@ -10,20 +10,40 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
+// var binaryTreePaths = function(root) {
+//     let ans = [];
+   
+//     function traverse( node, nodes ) {
+//         if( !node.left && !node.right )
+//             ans.push( [ ...nodes, node.val ].join('->') );
+           
+//         if( node.left )
+//             traverse( node.left, [ ...nodes, node.val ] );
+    
+//          if( node.right )
+//             traverse( node.right, [ ...nodes, node.val ] );
+//     }
+    
+//     traverse( root, [] );
+//     return ans;
+// };
+
 var binaryTreePaths = function(root) {
-    let ans = [];
+    let ans = [], result = []
    
     function traverse( node, nodes ) {
+        ans.push(node.val);
         if( !node.left && !node.right )
-            ans.push( [ ...nodes, node.val ].join('->') );
-           
+            result.push( ans.join('->') );
+          
         if( node.left )
-            traverse( node.left, [ ...nodes, node.val ] );
+            traverse( node.left);
     
          if( node.right )
-            traverse( node.right, [ ...nodes, node.val ] );
+            traverse( node.right );
+        ans.pop();
     }
     
-    traverse( root, [] );
-    return ans;
+    traverse( root );
+    return result;
 };
