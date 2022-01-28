61f0f63b7362c81466bd0ed51b094bafb55c4a64
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-28 17:22:15 +0530

    Time: 84 ms (96.19%), Space: 49.2 MB (5.24%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,32 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    let queue = [ root ];
+    
+    while( queue[0] ) {
+        let qLength = queue.length;
+        
+        for( let i=0; i<qLength; i++ ) {
+            let current = queue.shift();
+            
+            if( i < qLength -1 )
+                current.next = queue[0];
+            
+            if( current.left ) queue.push(current.left);
+            if( current.right ) queue.push(current.right);
+        }
+    }
+    return root;
+};
