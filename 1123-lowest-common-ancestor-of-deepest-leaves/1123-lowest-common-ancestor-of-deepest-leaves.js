d157804d51a1c1684eaf1582283b4238a4a143c6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-29 22:08:22 +0530

    Time: 146 ms (21.05%), Space: 46.5 MB (6.14%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.js
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.js
@@ -18,28 +18,28 @@ var lcaDeepestLeaves = function(root) {
     function findMaxDepth( node, d ) {
         	if(!node)
 		return 
-	maxDepth = Math.max(maxDepth, d)
-	findMaxDepth( node.left, d+1 );
-	findMaxDepth( node.right, d+1 );
+        maxDepth = Math.max(maxDepth, d)
+        findMaxDepth( node.left, d+1 );
+        findMaxDepth( node.right, d+1 );
     }
 	
     function helper( node, d ) {
-if( !node )
-	return null
+        if( !node )
+            return null
 
-if( d === maxDepth )
-	return node
-	
-	let l = helper(node.left, d+1);
-	let r = helper( node.right, d+1 );
+        if( d === maxDepth )
+            return node
 
-if( !l && !r )
-	return null
+            let l = helper(node.left, d+1);
+            let r = helper( node.right, d+1 );
 
-if( l && r )
-	return node
-	
-	if ( !l ||  !r )
-		return l ? l  : r
+        if( !l && !r )
+            return null
+
+        if( l && r )
+            return node
+
+            if ( !l ||  !r )
+                return l ? l  : r
     }
 };
