190932713244a2322ae798c687020e408f6f5228
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-29 11:50:01 +0530

    Time: 134 ms (6.11%), Space: 44.2 MB (6.11%) - LeetHub

---
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var flipEquiv = function(root1, root2) {
+    
+    
+    if( !root1 && !root2 )
+        return true
+    
+    if( !root1 || !root2 || root1.val !== root2.val )
+        return false
+    
+    let firstCheck = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+    let secondCheck = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+    
+    return root1.val === root2.val && (firstCheck || secondCheck);
+};
