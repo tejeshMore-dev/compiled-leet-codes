054adfcf5e31533659eda2c5b3562804e4ab8a5d
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-29 14:38:38 +0530

    Time: 136 ms (30.70%), Space: 47.2 MB (6.14%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var lcaDeepestLeaves = function(root) {
+    let maxDepth = 1;
+    findMaxDepth( root, 1 );
+    return helper( root, 1 );    
+
+    function findMaxDepth( node, d ) {
+        	if(!node)
+		return 
+	maxDepth = Math.max(maxDepth, d)
+	findMaxDepth( node.left, d+1 );
+	findMaxDepth( node.right, d+1 );
+    }
+	
+    function helper( node, d ) {
+if( !node )
+	return null
+
+if( d === maxDepth )
+	return node
+	
+	let l = helper(node.left, d+1);
+	let r = helper( node.right, d+1 );
+
+if( !l && !r )
+	return null
+
+if( l && r )
+	return node
+	
+	if ( !l ||  !r )
+		return l ? l  : r
+    }
+};
