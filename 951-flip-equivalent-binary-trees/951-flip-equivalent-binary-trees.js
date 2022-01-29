09ca058ab19fe332725f5bf1c1c9b22f744ddbc0
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-01-29 11:50:26 +0530

    Time: 117 ms (21.67%), Space: 44.8 MB (6.11%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.js
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.js
@@ -17,7 +17,7 @@ var flipEquiv = function(root1, root2) {
     if( !root1 && !root2 )
         return true
     
-    if( !root1 || !root2 || root1.val !== root2.val )
+    if( !root1 || !root2 )
         return false
     
     let firstCheck = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
