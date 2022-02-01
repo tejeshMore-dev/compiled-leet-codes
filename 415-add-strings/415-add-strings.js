5bdc13f0c0a7e7d7dbe1a7ec19897b74dde6ede1
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-02 00:20:52 +0530

    Time: 350 ms (5.00%), Space: 51.4 MB (5.00%) - LeetHub

---
+++ 415-add-strings/415-add-strings.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let c = 0, ans = [], l1 = num1.length-1, l2 = num2.length-1;
+    
+    while( c || l1 >= 0 || l2 >= 0 ) {
+        let sum = (l1 < 0 ? 0 : getNum(num1[l1])) +
+                  (l2 < 0 ? 0 : getNum(num2[l2])) + 
+                  c;
+        console.log(getNum(num2[l2]), getNum(num1[l1]), c, sum)
+        if(sum > 9) {
+            ans.push( sum%10 );
+            c = Math.floor(sum/10);
+        } else {
+            ans.push( sum );
+            c = 0;
+        }
+        l1--;
+        l2--
+    }
+    ans.reverse();
+    return ans.join("");
+    
+    
+    function getNum(num) {
+        return num - '0'
+    }
+};
