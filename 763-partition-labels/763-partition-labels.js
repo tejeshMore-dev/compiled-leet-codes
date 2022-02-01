d8f06a7e3bad21ed12466274079915c6d74866a6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-01 23:16:23 +0530

    Time: 96 ms (55.13%), Space: 44.7 MB (7.45%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var partitionLabels = function(s) {
+	let map = {}, res = [];
+
+	for( let i = 0; i < s.length; i++ )  {
+		let char = s.charAt(i);
+		
+        if( !map[char] ) {
+            map[char] = { f: i, l:i  }	
+        } else {
+            map[char].l = i;
+        }
+    }
+	
+	let lp = 0 , c = 0, max = -1;
+	
+    while( c < s.length ) {
+        let char = s.charAt(c);
+        let { l , f } = map[char];
+        
+        if( l > max )
+            max = l 
+        
+		if( c == max ) {
+            res.push(max-lp+1);
+            lp = c+1;
+            max = -1;
+            c++;
+            continue
+        }
+        c++
+    }
+    
+    return res
+}
