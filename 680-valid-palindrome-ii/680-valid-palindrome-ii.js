5d5baf951b2313b7739f212a90ca359640de6aee
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-01 23:08:24 +0530

    Time: 80 ms (97.71%), Space: 50.4 MB (14.45%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.js
@@ -6,21 +6,18 @@ var validPalindrome = function(s) {
 	let count = 0;
 	return isPalindrome( 0, s.length-1 );
 	
-	// check for palindrome 
+	// check for isPalindrome 
 	function isPalindrome ( lp, rp ) {
-        if ( lp >=  rp ) {
+        if ( lp >=  rp )
             return true 	
-        }
         
-        if( s.charAt(lp) === s.charAt(rp)  ){
-            return isPalindrome(lp+1, rp -1);
-        } else {
-            if(count < 1) {
-                count++;
+        if( s.charAt(lp) === s.charAt(rp) ) 
+            return isPalindrome(lp+1, rp-1);
+        else {
+            if(count++ < 1) 
                 return isPalindrome(lp + 1, rp) || isPalindrome(lp, rp-1)
-            } else {
+            else 
                 return false
-            }
         }
     }
 	
