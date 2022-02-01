d790bbaa7a6a501ff6d2b98c73e7558b1a95cebb
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-01 22:25:51 +0530

    Time: 110 ms (27.80%), Space: 44.3 MB (5.25%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.js
@@ -0,0 +1,78 @@
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function(words, order) {
+    // create order mapping
+    let map = {};
+        for( let i = 0; i < order.length; i++ ) {
+            map[order.charAt(i)] = i
+    }
+    
+    // compare words
+    for( let i = 0 ; i < words.length-1; i++ ) {
+        if( !isSorted(words[i], words[i+1]) ) {
+        return false	
+        }	
+    }
+    return true
+
+    // helper function
+    function isSorted( a, b ) {
+        for( let i = 0; i < a.length && i < b.length; i++ ) {
+            if( map[b.charAt(i)] >  map[a.charAt(i)] )
+                return true
+            else if( map[b.charAt(i)] <  map[a.charAt(i)] )
+                return false  
+                
+        }
+            return a.length <= b.length
+    }
+};
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
