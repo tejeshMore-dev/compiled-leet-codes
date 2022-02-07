112091327506e03669e9847683e534601778b2b7
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-08 00:57:19 +0530

    Time: 98 ms (51.23%), Space: 44 MB (10.56%) - LeetHub

---
+++ 78-subsets/78-subsets.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    return helper( 0, [], [] );
+    
+    function helper(i, temp, ans) {
+        if( i === nums.length ) {
+            ans.push( temp )
+            return
+        }
+        
+        helper( i+1, [ ...temp, nums[i] ], ans );
+        helper( i+1,temp, ans );
+        
+        return ans 
+    }
+    
+    
+    
+    
+    
+    
+    
+};
+
+/*
+
+[] [1] 
+[] [2] [1] [12]
+[] [3] [2] [23] [1] [13] [12] [123] 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
