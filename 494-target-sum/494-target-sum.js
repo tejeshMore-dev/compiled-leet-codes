de7f57aff00bda8e1435dfac5cce285766c0932c
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-07 23:25:46 +0530

    Time: 307 ms (73.10%), Space: 50.6 MB (12.69%) - LeetHub

--- 494-target-sum/494-target-sum.js
+++ 494-target-sum/494-target-sum.js
@@ -8,21 +8,18 @@ var findTargetSumWays = function(nums, target) {
     return helper( 0, 0 );
     
     function helper( i, sum ) {        
-        if( i === nums.length ) {
-            return sum === target ? 1 : 0            
-        }
-        
         if( map.has(`${i}_${sum}`) ) {
             return map.get(`${i}_${sum}`)
         }
         
+        if( i === nums.length ) {
+            return sum === target ? 1 : 0            
+        }      
         let num = nums[i];
         let opt1 = helper( i+1, sum + num );
         let opt2 = helper( i+1, sum - num );
         
         map.set(`${i}_${sum}`, opt1 + opt2);
-        
         return opt1 + opt2
-        
     }
 };
