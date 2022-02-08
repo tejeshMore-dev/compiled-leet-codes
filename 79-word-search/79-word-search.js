7b11ecb2be5a47c937de4200a9db413a89bac137
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-08 13:21:53 +0530

    Time: 3735 ms (6.24%), Space: 48.1 MB (13.79%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -19,8 +19,8 @@ var exist = function(board, word) {
     return false
     
     function backtrack( r, c, i ) {
-        // if( cache[`${r}-${c}-${i}`] )
-        //     return cache[`${r}-${c}-${i}`]
+        if( cache[`${r}-${c}-${i}`] )
+            return cache[`${r}-${c}-${i}`]
         
         if( i === word.length )
             return true
@@ -34,53 +34,9 @@ var exist = function(board, word) {
         let res = backtrack( r-1, c, i + 1 ) || backtrack( r+1, c, i + 1 ) || backtrack( r, c-1, i + 1 ) || backtrack( r, c+1, i + 1 )
         board[r][c] = temp;        //setting back the value from '*' to the letter
         
+        cache[`${r}-${c}-${i}`]  = res;
         return res
     }
 };
 
 
-// var exist = function(board, word) {
-//     if(board == null || word == null || board.length == 0) //edge case
-//         return false;
-    
-    
-//     for(let row = 0; row < board.length; row++){
-//         for(let col = 0; col < board[0].length; col++){
-//             if(helper(board, word, row, col, 0))  //recursive check
-//                 return true;
-//         }
-//     }
-    
-    
-//     function helper(board, word, row, col, wordIndex){
-        
-//         if(wordIndex == word.length) 
-//             return true;
-        
-//         //out of bounds check
-//         if(row < 0 || row >= board.length || col < 0  || col >= board[0].length)
-//             return false;
-        
-//         //letter check in the box (if the letter at the board is not equal to the letter of the given word return false)
-//         if(board[row][col] != word[wordIndex])
-//             return false;
-        
-//         //choose
-//         let temp = board[row][col];
-//         board[row][col] = '*';          //marking the visited box
-        
-//         //explore
-//         let bool = helper(board, word, row-1, col, wordIndex+1) ||
-//             helper(board, word, row+1, col, wordIndex+1) ||
-//             helper(board, word, row, col-1, wordIndex+1) ||
-//             helper(board, word, row, col+1, wordIndex+1);
-    
-//         //unchoose
-//         board[row][col] = temp;        //setting back the value from '*' to the letter
-        
-//         return bool;
-//     }
-    
-    
-//     return false;
-// };
