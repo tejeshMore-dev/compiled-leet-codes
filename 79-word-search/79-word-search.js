a6ef4f6b1898b48cc60449bbcd2c1bba0bb22dbf
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-08 13:21:00 +0530

    Time: 515 ms (62.57%), Space: 42.8 MB (52.78%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -11,28 +11,29 @@ var exist = function(board, word) {
     
     for( let r = 0; r < rows; r++ ) {
         for( let c = 0; c < cols; c++ ) {
-            if( backtrack( r, c, 0, new Set() ) ) {
+            if( backtrack( r, c, 0 ) ) {
                 return true
             }
         }
     }
     return false
     
-    function backtrack( r, c, i, set ) {
+    function backtrack( r, c, i ) {
         // if( cache[`${r}-${c}-${i}`] )
         //     return cache[`${r}-${c}-${i}`]
         
         if( i === word.length )
             return true
         
-        if( r < 0 || c < 0 || r >= rows || c >= cols || set.has(`${r}-${c}`) || board[r][c] !== word.charAt(i)  )
+        if( r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] !== word.charAt(i)  )
             return false
         
-        set.add(`${r}-${c}`);
-        let res = backtrack( r-1, c, i + 1, set ) || backtrack( r+1, c, i + 1, set ) || backtrack( r, c-1, i + 1, set ) || backtrack( r, c+1, i + 1, set )
-        set.delete(`${r}-${c}`);
+        let temp = board[r][c];
+        board[r][c] = '*';          //marking the visited box
         
-        cache[`${r}-${c}-${i}`] = res;
+        let res = backtrack( r-1, c, i + 1 ) || backtrack( r+1, c, i + 1 ) || backtrack( r, c-1, i + 1 ) || backtrack( r, c+1, i + 1 )
+        board[r][c] = temp;        //setting back the value from '*' to the letter
+        
         return res
     }
 };
