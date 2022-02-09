7e4bac0bbfcec511fa8c8fd67456431e6f7ed8eb
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 17:56:45 +0530

    Time: 2228 ms (5.04%), Space: 171.9 MB (5.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,25 +4,24 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    let map = new Map();
-    return helper( text1, text2, text1.length-1, text2.length-1, map )
+    return helper( text1, text2, text1.length-1, text2.length-1, {} )
 };
 
-function helper( text1, text2, n, m, map ) {
+function helper( text1, text2, n, m, cache ) {
     if( n < 0 || m < 0 )
         return 0;
 
-    if( map.has(`${n}-${m}`) )
-        return map.get(`${n}-${m}`)
+    if( cache[`${n}-${m}`] )
+        return cache[`${n}-${m}`]
 
     let res;
 
     if( text1.charAt(n) === text2.charAt(m) ){
-        res = 1 + helper( text1, text2, n-1, m-1, map );            
+        res = 1 + helper( text1, text2, n-1, m-1, cache );            
     } else {
-        res = Math.max( helper( text1, text2, n, m-1, map ), helper( text1, text2, n-1, m, map ) );
+        res = Math.max( helper( text1, text2, n, m-1, cache ), helper( text1, text2, n-1, m, cache ) );
     }
 
-    map.set(`${n}-${m}`, res);
+    cache[`${n}-${m}`] = res;
     return res;
 }
