5c875d6216024dd0359d23dcdbc3ee389b6e91ff
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 09:56:15 +0530

    Time: 145 ms (46.68%), Space: 45.5 MB (20.70%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
@@ -11,7 +11,11 @@ var lengthOfLongestSubstringKDistinct = function(s, k) {
     
     while( rp < s.length ) {
         if( !map[s.charAt(rp)] ) {
+            // if new char and distinct chars == k 
             if( counter === k ) {
+                // remove char one by one from left (i.e lp), 
+                // if any char count becomes 0, then need to delete it from hashmap and decrement distinct chars counter
+                // once distinct chars counter decremented, we can have new distinct chat in sliding window
                 while( counter >= k ) {
                     map[s.charAt(lp)] = map[s.charAt(lp)] - 1;
                     if( map[s.charAt(lp)] === 0 ) {
@@ -22,13 +26,14 @@ var lengthOfLongestSubstringKDistinct = function(s, k) {
                 }
             }
             
-            counter++;
+            // if new char but distinct chars < k 
+            counter++; // increment distinct chars counter
             map[s.charAt(rp)] = 0;
         }
         
-        map[s.charAt(rp)]  = map[s.charAt(rp)] + 1;
+        map[s.charAt(rp)]  = map[s.charAt(rp)] + 1; // increase frequency in hashmap
         ans = Math.max( ans, rp-lp+1 );
-        rp++
+        rp++;
     }
     
     return ans;
