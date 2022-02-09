3ccb1245feed31424dfe4a92e3f7311fd68444a4
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 15:40:24 +0530

    Time: 2180 ms (5.18%), Space: 117.1 MB (11.65%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
@@ -3,31 +3,21 @@
  * @return {number}
  */
 var longestPalindromeSubseq = function(s) {
-    let len = s.length-1;
-    let cache= {};
-    let rs = s.split("");
-    
-    let lp = 0, rp = rs.length-1;
-     while( lp < rp ) {
-         let temp = rs[lp];
-         rs[lp++] = rs[rp];
-         rs[rp--] = temp;
-     }
-    rs = rs.join("");
-    return helper( s.length-1, rs.length-1, new Map() );
+    let len = s.length;
+    return helper( 0, s.length-1, new Map() );
     
     function helper( m, n, map ) {
        if( map.has(`${m}-${n}`) )
             return map.get(`${m}-${n}`);
         
-        if( m < 0 || n < 0 )
+        if( m === len || n < 0 )
             return 0
         
         let res;
-        if( s.charAt(m) === rs.charAt(n) ) {
-            res = 1 + helper( m-1, n-1, map )
+        if( s.charAt(m) === s.charAt(n) ) {
+            res = 1 + helper( m+1, n-1, map )
         } else {
-            res = Math.max( helper(m-1, n, map), helper(m, n-1, map) )
+            res = Math.max( helper(m+1, n, map), helper(m, n-1, map) )
         }
         
         map.set(`${m}-${n}`, res);
