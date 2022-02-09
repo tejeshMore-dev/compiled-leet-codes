d33566069ead36a8ca7fadc357cad894dd2b4ed2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 19:56:14 +0530

    Time: 2131 ms (5.04%), Space: 171.3 MB (5.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -19,7 +19,8 @@ function helper( text1, text2, n, m, cache ) {
     if( text1.charAt(n) === text2.charAt(m) ){
         return cache[`${n}-${m}`] = 1 + helper( text1, text2, n-1, m-1, cache );            
     } else {
-        return cache[`${n}-${m}`] = Math.max( helper( text1, text2, n, m-1, cache ), helper( text1, text2, n-1, m, cache ) );
+        return cache[`${n}-${m}`] = Math.max( helper( text1, text2, n, m-1, cache ), 
+                                             helper( text1, text2, n-1, m, cache ) );
     }
     
 }
