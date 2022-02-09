ee543b0c96ca1bdf5d444eb5de355b256ac37671
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 14:51:26 +0530

    Time: 2176 ms (5.18%), Space: 117.3 MB (11.65%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
@@ -0,0 +1,80 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindromeSubseq = function(s) {
+    let len = s.length-1;
+    
+    let rs = s.split("");
+    
+    let lp = 0, rp = rs.length-1;
+     while( lp < rp ) {
+         let temp = rs[lp];
+         rs[lp++] = rs[rp];
+         rs[rp--] = temp;
+     }
+    rs = rs.join("");
+    return helper( s.length-1, rs.length-1, new Map() );
+    
+    function helper( m, n, map ) {
+       if( map.has(`${m}-${n}`) )
+            return map.get(`${m}-${n}`);
+        
+        if( m < 0 || n < 0 )
+            return 0
+        
+        let res;
+        if( s.charAt(m) === rs.charAt(n) ) {
+            res = 1 + helper( m-1, n-1, map )
+        } else {
+            res = Math.max( helper(m-1, n, map), helper(m, n-1, map) )
+        }
+        
+        map.set(`${m}-${n}`, res);
+        return res
+    }
+};
+/*
+
+
+5  bbbab
+5  babbb
+
+len-1-i
+4 3
+
+0 1
+b b b b 
+
+
+
+cbbd
+dbbc
+bb 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
