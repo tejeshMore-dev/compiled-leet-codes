b487c3b262b5376da21a9315402518e560b345e2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 23:44:44 +0530

    Time: 84 ms (98.03%), Space: 47.1 MB (26.20%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let lp = 0, rp = 0, ans = 0, set = new Set();
+    
+    while( rp < s.length ) {
+        if( set.has(s.charAt(rp)) ) {
+
+            while( set.has(s.charAt(rp)) ) {
+                delete set.delete(s.charAt(lp))
+                lp++
+            }
+            
+        }
+        
+        set.add(s.charAt(rp)); 
+        ans = Math.max( ans, rp-lp+1 )
+        rp++;
+    }
+    
+    return ans;
+};
