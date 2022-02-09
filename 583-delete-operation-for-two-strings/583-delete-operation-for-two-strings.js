d1f0554d8b993c07e2bf58920d53e0db2e4ac7de
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 19:24:15 +0530

    Time: 479 ms (10.24%), Space: 55.6 MB (9.45%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
@@ -4,29 +4,34 @@
  * @return {number}
  */
 // var minDistance = function(word1, word2) {
-//     let i1L = word1.length, i2L = word2.length;
-//     return helper( 0, 0, new Map() );
-    
-//     function helper(i1, i2, map) {            
-//         if( i2 === i2L ) 
-//             return i1L - i1
-    
-//         if( i1 === i1L ) 
-//             return i2L - i2
-        
-//         if( map.has(`$[i1}-${i2}`) )
-//             return map.get(`$[i1}-${i2}`)
-        
-//         let res;
-//         if ( word1.charAt(i1) === word2.charAt(i2) )
-//             res = helper( i1+1, i2+1, map )
-//         else
-//             res = Math.min( helper(i1+1, i2, map), helper(i1, i2+1, map) ) + 1
-        
-//         map.set( `$[i1}-${i2}`, res );
-//         return res;
-//     }
+//     return helper( word1, word2, 0, 0, {} );
 // };
+
+// function helper(w1, w2, i1, i2, cache) {      
+//         if( w2.length === i2 ){
+//             return w1.length - i1            
+//         }
+    
+//         if( w1.length === i1 ) {
+//             return w2.length - i2            
+//         }
+    
+//           if( cache[`$[i1}-${i2}`] !== undefined ) {
+//             return cache[`$[i1}-${i2}`]
+                          
+//           }
+        
+//         if ( w1[i1] === w2[i2] ){
+//             cache[`$[i1}-${i2}`] = helper( w1, w2, i1+1, i2+1, cache )
+//             return cache[`$[i1}-${i2}`];
+//         }
+//         else {
+//             let min = Math.min( helper(w1, w2, i1+1, i2, cache), 
+//                                             helper(w1, w2, i1, i2+1, cache) );
+//             cache[`$[i1}-${i2}`] = min + 1;
+//            return cache[`$[i1}-${i2}`];
+//         }
+// }
 
 var minDistance = function(word1, word2) {
     var json = {};
