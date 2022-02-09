ea1009c0c38b7e7c6ca95b5725e35e553b7d0ef7
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-09 19:45:05 +0530

    Time: 415 ms (12.60%), Space: 54.9 MB (11.02%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
@@ -4,11 +4,10 @@
  * @return {number}
  */
 var minDistance = function(word1, word2) {
-    let json = {}
-    return helper( word1, word2, 0, 0, json );
+    return helper( word1, word2, 0, 0, {} );
 };
 
-function helper(w1, w2, i1, i2, json) {      
+function helper(w1, w2, i1, i2, cache) {      
         if( w2.length === i2 ){
             return w1.length - i1;            
         }
@@ -17,18 +16,17 @@ function helper(w1, w2, i1, i2, json) {
             return w2.length - i2;           
         }
     
-          if( json[`${i1}-${i2}`] !== undefined ) {
-            return json[`${i1}-${i2}`]
+          if( cache[`${i1}-${i2}`] !== undefined ) {
+            return cache[`${i1}-${i2}`]
                           
           }
         
         if ( w1[i1] === w2[i2] ){
-            return json[`${i1}-${i2}`] = helper( w1, w2, i1+1, i2+1, json );
+            return cache[`${i1}-${i2}`] = helper( w1, w2, i1+1, i2+1, cache );
         }
         else {
-            return json[`${i1}-${i2}`] = Math.min( helper(w1, w2, i1+1, i2, json), 
-                                                  helper(w1, w2, i1, i2+1, json) ) + 1;
-           
+            return cache[`${i1}-${i2}`] = Math.min( helper(w1, w2, i1+1, i2, cache), 
+                                                  helper(w1, w2, i1, i2+1, cache) ) + 1;
         }
 }
 // var minDistance = function(word1, word2) {
