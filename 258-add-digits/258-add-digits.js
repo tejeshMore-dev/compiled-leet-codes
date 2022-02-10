10feadd060ab1fa1ea75f7b7289d9c58790ab727
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-10 21:52:59 +0530

    Time: 94 ms (67.07%), Space: 44.3 MB (12.37%) - LeetHub

---
+++ 258-add-digits/258-add-digits.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    let ans = 0;
+    
+    while( num ) {
+        ans += num%10;
+        num = Math.floor( num/10 );
+        
+        if( ans > 9 && num === 0 ) {
+            num = ans;
+            ans= 0
+        }
+    }
+    
+    return ans;
+};
+/*
+
+
+248
+2 4 8
+1 4
+5
+
+
+14 >> 5
+38 >> 11 2
+
+
+
+
+
+*/
