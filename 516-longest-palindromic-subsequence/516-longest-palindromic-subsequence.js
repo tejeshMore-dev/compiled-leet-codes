2105b6642b30cf1d5f38339a36de3163042f6874
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-11 09:43:53 +0530

    Time: 240 ms (90.73%), Space: 71.9 MB (69.97%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.js
@@ -2,27 +2,46 @@
  * @param {string} s
  * @return {number}
  */
+
+// var longestPalindromeSubseq = function(s) {
+//   var sols = new Array(s.length).fill(0).map(a => new Array(s.length).fill(-1));
+//     return helper( s, 0, s.length-1, sols );
+// };
+
+// function helper( s, l, r, sols ) {
+//     if (sols[l][r] !== -1) return sols[l][r];
+
+
+//     if( l === r )
+//         return 1
+    
+//     if( l > r )
+//         return 0
+    
+
+//     if( s.charAt(l) === s.charAt(r) ) {
+//         return sols[l][r] = 2 + helper( s, l+1, r-1, sols );
+//     } else {
+//         return sols[l][l] = Math.max( helper(s, l, r-1, sols), helper(s, l+1, r, sols), helper(s, l+1, r-1, sols) )
+//     }
+// }
+
 var longestPalindromeSubseq = function(s) {
-    let cache = {};
-    return helper( s, 0, s.length-1, cache );
-};
-
-function helper( s, l, r, cache ) {
-   if( cache[`${l}-${r}`] )
-        return cache[`${l}-${r}`];
-
-    if( l === r )
-        return 1
-    
-    if( l > r )
-        return 0
-    
-
-    if( s.charAt(l) === s.charAt(r) ) {
-        return cache[`${l}-${r}`] = 2 + helper( s, l+1, r-1, cache );
+  var sols = new Array(s.length).fill(0).map(a => new Array(s.length).fill(-1));
+    return recursion(s, 0, s.length - 1, sols); 
+}
+function recursion(s, i, j, sols) {
+    if (sols[i][j] !== -1) return sols[i][j];
+    if (i === j) return 1;
+    if (i  > j) return 0;
+    if (s[i] === s[j]) {
+        var val = 2 + recursion(s, i + 1, j - 1, sols);
+        sols[i][j] = val;
+        return sols[i][j];
     } else {
-        cache[`${l}-${r}`] = Math.max( helper(s, l, r-1, cache), helper(s, l+1, r, cache), helper(s, l+1, r-1, cache) )
-        return cache[`${l}-${r}`];
+        var max = Math.max(recursion(s, i, j - 1, sols),recursion(s, i+1, j, sols),recursion(s, i+1, j - 1, sols));
+         sols[i][j] = max;
+        return sols[i][j];
     }
 }
 /*
