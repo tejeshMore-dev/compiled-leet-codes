338fa936f443f9e4ee7b664f9cc3fde364667dc3
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-12 14:26:17 +0530

    Time: 60 ms (95.11%), Space: 42.3 MB (5.78%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.js
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.js
@@ -10,11 +10,10 @@ var kthGrammar = function(n, k) {
 function helper( n, k ) {
     if( n === 1  )
         return 0
-    let len = 2 ** (n-1);
-    
-    if( k <= (len /2) ) {
+    let mid = (2 ** (n-1))/2;
+    if( k <= mid ) {
         return helper( n-1, k )
     } else {
-        return helper( n-1, (k-(len/2)) ) ? 0 : 1
+        return helper( n-1, k-mid ) ? 0 : 1
     } 
 }
