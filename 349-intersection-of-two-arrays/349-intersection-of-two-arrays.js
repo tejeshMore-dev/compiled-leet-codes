234d9914774e887e705fe14fc821b000a34b31de
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-13 03:11:49 +0530

    Time: 97 ms (46.83%), Space: 44.5 MB (5.13%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -7,23 +7,15 @@ var intersection = function(nums1, nums2) {
     let ans = [], map = {};
     
     for( let num of nums1 ) {
-        if( !map[num] )
-            map[num] = { l1: true, l2: false };
-        else 
-            map[num].l1 = true;
+        map[num] = 1;
     }
     
     for( let num of nums2 ) {
-        if( !map[num] )
-            map[num] = { l1: false, l2: true };
-        else 
-            map[num].l2 = true;
-    }
-    
-    for( let num in map ) {
-        if( map[num].l1 && map[num].l2 )
+        if( map[num] ) {
             ans.push(num)
+            map[num] = 0;            
+        }
     }
-    
+
     return ans
 };
