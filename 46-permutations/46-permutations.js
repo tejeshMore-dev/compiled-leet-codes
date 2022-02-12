45ece608c0c8659338afdf1b354b471ac670ce01
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-13 01:58:48 +0530

    Time: 98 ms (62.32%), Space: 44 MB (29.89%) - LeetHub

---
+++ 46-permutations/46-permutations.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    return dfs( nums, [], Array(nums.length).fill(false), [] )
+};
+
+function dfs( nums, p, used, res ) {
+    if( p.length === nums.length ) {
+        res.push( Array.from(p) )
+        return
+    }
+    
+    for( let i = 0; i < nums.length; i++ ) {
+        if( used[i] )
+            continue;
+        
+        p.push(nums[i]);
+        used[i] = true;
+        
+        dfs(nums, p, used, res);
+        
+        p.pop();
+        used[i] = false;
+        // dfs(nums, p, used, res);
+    }    
+    return res;
+}
+/*
+123
+
+123
+[123] [231] [  ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
