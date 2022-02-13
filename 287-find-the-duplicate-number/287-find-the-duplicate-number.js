9c2a72458a40e14883a4e86440f4262fed181783
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-14 05:24:03 +0530

    Time: 121 ms (71.04%), Space: 49.7 MB (58.88%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -2,22 +2,41 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
-    let sp = nums[0], fp = nums[nums[0]];
-    
-    while( sp !== fp ) {
-        sp = nums[sp];
-        fp = nums[nums[fp]];
-    }
-    
-    fp = 0;
-     while( sp !== fp ) {
-        sp = nums[sp];
-        fp = nums[fp];
-    }
-    
-    return sp;
-};
 
+// var findDuplicate = function(nums) {
+//     let sp = nums[0], fp = nums[nums[0]];
+    
+//     while( sp !== fp ) {
+//         sp = nums[sp];
+//         fp = nums[nums[fp]];
+//     }
+    
+//     fp = 0;
+//      while( sp !== fp ) {
+//         sp = nums[sp];
+//         fp = nums[fp];
+//     }
+    
+//     return sp;
+// };
+
+var findDuplicate = function(nums) {
+    let duplicate = -1;
+    
+    for( let i = 0; i < nums.length; i++ ) {
+        let val = Math.abs(nums[i]);
+        if( nums[val] < 0 ) {
+            duplicate = val
+            break
+        }
+        nums[val] = nums[val] * -1
+    }
+    
+    for( let i = 0; i < nums.length; i++ ) {
+        nums[i] = Math.abs(nums[i]);
+    }
+    
+    return duplicate;
+}
 
 
