cd79c70c53571a62ac8f3610843a987b4c928dd2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-13 18:45:17 +0530

    Time: 441 ms (75.78%), Space: 42.7 MB (59.85%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -7,34 +7,36 @@
 
 var exist = function(board, word) {
     let rows = board.length, cols = board[0].length;
+    
     for( let r = 0; r < rows; r++ ) {
-        for( let c = 0; c < cols; c++ ) {
-            if( backtrack( r, c, 0 ) ) {
+        for( let c = 0 ; c < cols; c++ ) {
+            if( dfs(r, c, 0) )
                 return true
-            }
         }
     }
-    return false
     
-    function backtrack( r, c, i ) {   
-        
+    return false;
+    
+    function dfs( r, c, i ) {
         if( i === word.length )
-            return true
+            return true;
         
-        if( r < 0 || c < 0 || r >= rows || c >= cols || board[r][c] !== word.charAt(i)  )
-            return false
+        if( r < 0 || c < 0 || r >= rows || c >= cols || word[i] !== board[r][c] )
+            return false;
         
         let temp = board[r][c];
-        board[r][c] = '*';
+        board[r][c] = "*"
         
-        let res = backtrack( r-1, c, i + 1 ) || 
-                backtrack( r+1, c, i + 1 ) || 
-                backtrack( r, c-1, i + 1 ) || 
-                backtrack( r, c+1, i + 1 )
+        let res = dfs( r+1, c, i+1 ) || 
+            dfs( r-1, c, i+1 ) || 
+            dfs( r, c-1, i+1 ) || 
+            dfs( r, c+1, i+1 );
+
         board[r][c] = temp;
-        
-        return res
+        return res;        
     }
+    
 };
+
 
 
