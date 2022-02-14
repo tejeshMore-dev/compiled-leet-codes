282e68a8d73954d9c537bc0914a48a652f197da2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-14 20:16:06 +0530

    Time: 80 ms (72.68%), Space: 45.5 MB (7.65%) - LeetHub

---
+++ 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.js
@@ -0,0 +1,47 @@
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function ArrayReader() {
+ *
+ *     @param {number} index
+ *     @return {number}
+ *     this.get = function(index) {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {ArrayReader} reader
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (reader, target) {
+    if( reader.get(0) === target )
+        return 0;
+    
+    let l = 0, r = 1;
+    
+    while( reader.get(r) < target ) {
+        l = r;
+        r <<= 1;
+    console.log(l, r)
+    
+    }
+    let result = -1;
+    
+    while( l <= r ) {
+        let mid = Math.floor( l + (r-l)/2 );
+        
+        let midVal = reader.get(mid);
+        
+        if( midVal === target )
+            return result = mid;
+        else if( midVal > target )
+            r = mid-1
+        else
+            l = mid+1
+    }
+    
+    return result;
+};
