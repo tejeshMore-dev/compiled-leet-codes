bec5b29966ae742b9c0592f5813c672bc3bc45c5
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-14 21:14:02 +0530

    Time: 105 ms (29.17%), Space: 42.9 MB (6.69%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -6,13 +6,13 @@ var peakIndexInMountainArray = function(arr) {
     let lp = 0;
     let rp = arr.length-1;
     
-    while( lp <= rp ) {
-        let pivot = lp + Math.floor((rp-lp)/2);
+    while( lp < rp ) {
+        let pivot = Math.floor(lp+(rp-lp)/2);
         
-        if( arr[pivot] < arr[pivot + 1] )
+        if( arr[pivot+1] > arr[pivot] )
             lp = pivot + 1;
         else
-            rp = pivot - 1;
+            rp = pivot;
         
     }
     
