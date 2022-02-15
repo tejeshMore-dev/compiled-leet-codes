7748d0eb4907d278bfedfec163b883d5f7c3df4a
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-15 21:13:28 +0530

    Time: 80 ms (74.84%), Space: 46.9 MB (5.91%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let map = new Map(), result = [];
+    
+    for( let i = 0; i < s.length; i++ ) {
+        if( !map.has(s[i]) ) {
+            map.set(s[i], 1)
+        } else {
+            map.set(s[i], map.get(s[i])+1 );        
+        }
+    }
+    console.log(map)
+    
+    for( let i = 0; i < t.length; i++ ) {
+        if( map.has(t[i]) && map.get(t[i]) > 0 ) {
+            map.set(t[i], map.get(t[i]) - 1);
+        } else {
+            result.push(t[i])
+        }
+    }
+    console.log(map)
+    
+    return result.join("");
+};
