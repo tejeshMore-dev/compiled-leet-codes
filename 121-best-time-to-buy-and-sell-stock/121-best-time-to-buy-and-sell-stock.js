cb8eee5724f6c6979d6c40c5d8aa0f60e63d7347
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-15 08:56:07 +0530

    Time: 112 ms (61.99%), Space: 52 MB (18.78%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0], maxProfit = 0;
+    
+    for( let i = 1; i< prices.length; i++ ) {
+        if( prices[i] < minPrice )
+            minPrice = prices[i];
+        else
+            maxProfit = Math.max( maxProfit, prices[i] - minPrice )
+    }
+    
+    return maxProfit;
+};
