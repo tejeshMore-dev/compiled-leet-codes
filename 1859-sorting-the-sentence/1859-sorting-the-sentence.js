f7f4795c2f24870f3550d7b7be6acc3fefbf1074
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-17 00:33:25 +0530

    Time: 82 ms (52.39%), Space: 41.9 MB (29.98%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -6,14 +6,12 @@ var sortSentence = function(s) {
     let array = [], lp = 0;
 
     for( let i = 0; i < s.length; i++ ) {
-        if( Number.isInteger(parseInt(s.charAt(i))) ) {
+        if( !isNaN(s.charAt(i)) ) {
             array[ parseInt(s.charAt(i)) - 1 ] = s.substring( lp , i );
             i += 2;
             lp = i;
         }
-    // console.log(array)
         
     }
-    // console.log(array)
     return array.join(" ");
 };
