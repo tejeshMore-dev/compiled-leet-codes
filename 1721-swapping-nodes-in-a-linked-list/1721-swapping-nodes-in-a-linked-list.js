d3c63a43f564c6b45da38684b9e4c47ff81cdfc6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-16 20:25:58 +0530

    Time: 612 ms (43.91%), Space: 102.1 MB (27.24%) - LeetHub

---
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var swapNodes = function(head, k) {
+    let node = head, c = 0;
+    let node1, node2;
+    
+    while( node ) {
+        c++;        
+        node2 = node2 ? node2.next : null;
+        
+        if( c === k ) {
+            node1 = node;
+            node2 = head;            
+        }
+        
+        node = node.next;
+    }
+    // console.log(node1.val, node2.val);
+    let temp = node1.val;
+    node1.val = node2.val;
+    node2.val = temp;
+    
+    return head;
+};
