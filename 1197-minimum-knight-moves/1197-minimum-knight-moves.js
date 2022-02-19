74085d9fa6a38d311cd4f30c802577560cbc9af6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-19 14:12:55 +0530

    Time: 6364 ms (26.47%), Space: 143.8 MB (15.89%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var minKnightMoves = function(x, y) {
+    let directions = [ [-2, 1], [-1, 2], [-1, -2], [-2, -1], [2, 1], [1, 2], [1, -2], [2, -1]]; 
+    
+    let queue = [[0, 0]], minMoves = Number.MAX_VALUE, counter = 0;
+    let visited = new Set();
+    visited.add(`${0}-${0}`);
+    
+    while( queue.length > 0 ) {
+        let length = queue.length;
+        
+        for( let i = 0; i < length; i++ ) {
+            let [x1, y1] = queue.shift();
+        
+            if( x1 === x && y1 === y ) {
+                return minMoves = Math.min(minMoves, counter);
+            }
+
+            directions.map((direction) => {
+                let [xd, yd] = direction;
+                xd += x1;
+                yd += y1;
+                if( !visited.has(`${xd}-${yd}`) ){
+                    queue.push([xd, yd]);
+                    visited.add(`${xd}-${yd}`);
+                }
+
+            })
+        }
+
+        counter++;        
+    }
+    
+    return minMoves;
+};
