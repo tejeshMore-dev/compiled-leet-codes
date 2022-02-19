80e8022b2e9bcbf87e2088c8c1511f61a5bfcf39
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-19 05:53:10 +0530

    Time: 95 ms (24.24%), Space: 42.4 MB (24.24%) - LeetHub

--- 1134-armstrong-number/1134-armstrong-number.js
+++ 1134-armstrong-number/1134-armstrong-number.js
@@ -3,13 +3,14 @@
  * @return {boolean}
  */
 var isArmstrong = function(n) {
-    let sum = 0, ans = n;
+    let sum = 0, num = n;
     let k = Math.floor(Math.log10(n)+1);
     
-    while( n ) {
-        let r = n%10;
-        sum += r ** k;
-        n = Math.floor(n/10);        
+    while( num ) {
+        let r = num%10;
+        sum += ( r ** k );
+        num = Math.floor(num/10)
     }
-    return sum === ans
+    
+    return sum === n
 };
