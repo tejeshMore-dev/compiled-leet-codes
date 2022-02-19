d16c68532c6412144c8d61134b152feb85ab91fd
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-19 07:04:27 +0530

    Time: 88 ms (61.36%), Space: 42.5 MB (17.97%) - LeetHub

---
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var twoSumLessThanK = function(nums, k) {
+    nums.sort((a, b) => a - b );
+    let lp = 0, rp = nums.length-1, result = -1;
+    
+    while( lp < rp ) {
+        let sum = nums[lp] + nums[rp];
+        
+        if( sum < k ) {
+            lp++;
+            result = Math.max(result, sum);            
+        } else
+            rp--;
+    }
+    
+    return result
+
+};
