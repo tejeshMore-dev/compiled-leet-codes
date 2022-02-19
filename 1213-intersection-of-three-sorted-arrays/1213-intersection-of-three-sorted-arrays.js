4b27e3f80a14c91f28d20fea075f4b0d9d3daad2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-19 10:04:43 +0530

    Time: 87 ms (68.32%), Space: 43.1 MB (26.71%) - LeetHub

---
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number[]} arr3
+ * @return {number[]}
+ */
+var arraysIntersection = function(arr1, arr2, arr3) {
+    let p1 = 0, p2 = 0, p3 = 0;
+    let ans = [];
+    
+    while( p1 < arr1.length && p2 < arr2.length && p3 < arr3.length ) {
+        if( arr1[p1] === arr2[p2] && arr2[p2] === arr3[p3] ) {
+            ans.push(arr1[p1]);
+            p1++;
+            p2++;
+            p3++;
+            continue;
+        }
+        
+        let min = Math.min(arr1[p1], arr2[p2], arr3[p3]);
+        
+        if( arr1[p1] === min )
+            p1++;
+        
+        if( arr2[p2] === min )
+            p2++;
+        
+        if( arr3[p3] === min )
+            p3++;
+    }
+    
+    return ans;
+};
