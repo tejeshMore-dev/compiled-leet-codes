57d4c916e2b147e5c09530d257d4609d487f647c
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-20 10:50:26 +0530

    Time: 300 ms, Memory: 82.9 MB - LeetHub

---
+++ smallest-string-with-swaps/smallest-string-with-swaps.js
@@ -0,0 +1,67 @@
+/**
+ * @param {string} s
+ * @param {number[][]} pairs
+ * @return {string}
+ */
+var root
+
+class unionFind {
+    constructor(size) {
+        root = new Array(size);
+        
+        for( let i=0; i < size; i++ )
+            root[i] = i;
+    }
+    
+    find(x) {
+        if( x === root[x] )
+            return x
+        
+        return root[x] = this.find(root[x]);
+    }
+    
+    union(x, y) {
+        let rootX = this.find(x);
+        let rootY = this.find(y);
+        
+        if( rootX !== rootY ) {
+            root[rootY] = rootX
+        }
+    }
+}
+
+var smallestStringWithSwaps = function(s, pairs) {
+    const uf = new unionFind(s.length);
+    let map = {};
+    let ans = [];
+    
+    for( let i=0; i < pairs.length; i++ ) {
+        let [x, y] = pairs[i];
+        uf.union(x, y);
+    }
+    
+    for( let i=0; i < s.length; i++ ) {
+        if( !map[uf.find(i)] )
+            map[uf.find(i)] = [];
+        
+        map[uf.find(i)].push(s[i]);
+    }
+    
+    for( let key in map ) {
+        map[key].sort((a, b) => {
+            if (a < b) {
+            return 1;
+          }
+          if (a > b) {
+            return -1;
+          }
+        } );
+    }
+    
+    for( let i=0; i < s.length; i++ ) {
+        let p = uf.find(i);
+        ans.push(map[p].pop());
+    }
+    
+    return ans.join("");
+};
