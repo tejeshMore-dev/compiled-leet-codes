81f28f1d87bb3f21db30fa80b2c5ac23aaf0ca85
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-21 10:40:46 +0530

    Time: 60 ms (100.00%), Space: 45 MB (17.07%) - LeetHub

---
+++ 734-sentence-similarity/734-sentence-similarity.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string[]} sentence1
+ * @param {string[]} sentence2
+ * @param {string[][]} similarPairs
+ * @return {boolean}
+ */
+var areSentencesSimilar = function(sentence1, sentence2, similarPairs) {
+    if( sentence1.length !== sentence2.length )
+        return false;
+    
+    let map = {};
+    
+    for( let i=0 ; i<similarPairs.length; i++ ) {
+        let [ w1, w2 ] = similarPairs[i];
+        if( !map[w1] )
+            map[w1] = new Set();
+        
+        if( !map[w2] )
+            map[w2] = new Set();
+        
+        map[w1].add(w2);
+        map[w2].add(w1);
+        
+    }
+    
+    for( let i=0 ; i<sentence1.length; i++ ) {
+        let w1 = sentence1[i];
+        let w2 = sentence2[i];
+        
+        if( w1 === w2 )
+            continue;
+        else if( !(map[w1] && map[w1].has(w2) || 
+                   map[w2] && map[w2].has(w1) ) )
+            return false
+        
+    }
+    
+    return true;
+};
