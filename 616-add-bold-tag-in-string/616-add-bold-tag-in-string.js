0bfb753adbdcbd3d7c3cecf3fffbfae1436eb0bd
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-21 10:05:06 +0530

    Time: 136 ms (41.67%), Space: 44.7 MB (46.43%) - LeetHub

--- 616-add-bold-tag-in-string/616-add-bold-tag-in-string.js
+++ 616-add-bold-tag-in-string/616-add-bold-tag-in-string.js
@@ -7,6 +7,7 @@ var addBoldTag = function(s, words) {
     let boldI = new Array(s.length).fill(false);
     let end = -Number.MAX_VALUE;
     
+    // mark all index which are bold
     for( let i=0; i < s.length; i++ ) {
         for( let word of words ) {
             if( s.startsWith(word, i) )
@@ -17,17 +18,18 @@ var addBoldTag = function(s, words) {
     
     let sb = [];
     for( let i=0; i < s.length; i++ ) {
+        // if index not bold then use as is
         if( !boldI[i] ) {
             sb.push(s.charAt(i));
             continue;
         }
         
+        // if index in bold, then find end and
+        //push the substring with tag
         let end = i;
-        
         while( end < s.length && boldI[end] ) {
             end++;
         }
-        
         sb.push('<b>',s.substring(i, end), '</b>');
         i = end-1;
     }
