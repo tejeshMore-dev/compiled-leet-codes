60a3feeee97d923672c4c38c7ae53546261b3fa1
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-22 13:12:55 +0530

    Time: 84 ms (78.18%), Space: 44.3 MB (35.45%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var distanceK = function(root, target, k) {
+    let ans = [];
+    let map = {};
+    let visited = new Set();
+    
+    getParentMapping(root);
+    helper(target, k);
+    
+    return ans;
+    
+    function getParentMapping(node) {
+        if( !node )
+            return
+        
+        if( node.left ) {
+            map[node.left.val] = node;
+            getParentMapping(node.left);
+        }
+        
+        if( node.right ) {
+            map[node.right.val] = node;
+            getParentMapping(node.right);
+        }
+    }
+    
+    
+    function helper( node, k ) {
+        if( !node || visited.has(node.val) || k < 0 )
+            return null
+        
+        visited.add(node.val);
+        
+        if( k === 0 ) {
+            ans.push(node.val);
+        }
+        
+        if( node.left) {
+            helper( node.left, k-1 );
+        }
+        
+        if( node.right ) {
+            helper( node.right, k-1 );     
+        }
+        
+        if( map[node.val] ) {
+            helper( map[node.val], k-1 )            
+        }
+    }
+};
