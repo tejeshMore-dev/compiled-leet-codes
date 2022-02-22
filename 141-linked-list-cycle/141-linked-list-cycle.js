695007745dc9de271c0c4b8140ad41817c0c9cde
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-22 08:27:59 +0530

    Time: 110 ms (46.84%), Space: 44.7 MB (33.03%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let sp = head, fp = head;
+    
+    while( sp && sp.next && fp.next && fp.next.next ) {
+        sp = sp.next;
+        fp = fp.next.next;
+        
+        if( sp === fp )
+            return true
+    }
+    
+    return false
+};
