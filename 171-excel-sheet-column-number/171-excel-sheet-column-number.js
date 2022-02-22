7e3d3413de05445f4e11b4f3341c1fddc8772393
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-22 07:05:08 +0530

    Time: 125 ms (38.22%), Space: 44 MB (12.11%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.js
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.js
@@ -6,12 +6,12 @@ var titleToNumber = function(columnTitle) {
     let num = 0;
     
     for( let i in columnTitle ) {
-        num = num * 26 + getNumber(columnTitle.charAt(i));
+        num = num * 26 + _getNumber(columnTitle.charAt(i));
     }
     return num
     
   
-    function getNumber(char) {
+    function _getNumber(char) {
         return char.charCodeAt(0) - 'A'.charCodeAt(0) + 1;
     }
 };
