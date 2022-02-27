0848026f9425870e6e079f73fa73c8df764aa078
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-28 02:02:45 +0530

    Time: 92 ms, Memory: 50.6 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.js
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.js
@@ -3,31 +3,18 @@
  * @return {number[][]}
  */
 var allPathsSourceTarget = function(graph) {
-    const _graph = createGraph(graph);
+    return findAllPath( 0, graph.length-1, graph, [], [0] )
     
-    return dfs(_graph, 0, graph.length-1, [0], []);
+    function findAllPath(node, d, graph, ans, temp) {
+        if( node === d )
+            ans.push([ ...temp ])
+        
+        for( let nNode of graph[node] ) {
+            findAllPath(nNode, d, graph, ans, [ ...temp, nNode ]);
+        }
+        
+        return ans
+    }
 };
 
-function dfs(graph, curr, e, nodes, paths) {
-    if( curr === e ) {
-        paths.push( [ ...nodes ] );
-        return
-    }
-    for( let node of graph[curr] ) {                
-            nodes.push(node);
-            dfs(graph, node, e, nodes, paths)
-            nodes.pop();
-    }
-    
-    return paths;
-}
 
-function createGraph(graph) {
-    let map = {};
-    
-    for( let i=0; i < graph.length; i++ ) {
-        map[i] = [ ...graph[i] ];
-    }
-    
-    return map
-}
