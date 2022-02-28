d509781a2a25a7ab14a35bb79701a78aed86f0bd
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-02-28 19:48:34 +0530

    Time: 117 ms, Memory: 49.1 MB - LeetHub

---
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,39 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if( !root )
+        return root;
+    
+    let queue = [ root ];
+    
+    while( queue.length ) {
+        let qLen = queue.length;
+        
+        for( let i=0; i < qLen; i++ ) {
+            let current = queue.shift();
+            
+            if( i+1 < qLen )
+                current.next = queue[0];
+            
+            if( current.left )
+                queue.push(current.left);               
+            
+            if( current.right )
+                queue.push(current.right);
+        }
+    }
+    
+    return root
+};
