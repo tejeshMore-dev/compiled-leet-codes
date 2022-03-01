2502d6e84f40808573fac7ee3c45d9c900b9553d
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-02 05:14:06 +0530

    Time: 61 ms (90.12%), Space: 41.6 MB (44.95%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -3,18 +3,17 @@
  * @return {number}
  */
 var tribonacci = function(n) {
-    let cache = {};
-    return helper(n);
+    if( n == 0 || n == 1 || n == 2 )
+        return n == 0 ? 0 : 1;
     
-    function helper(n) {
-        if( n == 0 || n == 1 || n == 2 )
-            return n == 0 ? 0 : 1;
+    let t0 = 0, t1 = 1, t2 = 1;
     
-        if( cache.hasOwnProperty(n) )
-            return cache[n];
-        
-        let ans = helper(n-1) + helper(n-2) + helper(n-3);
-        cache[n] = ans;
-        return ans;
+    for( let i=3; i<=n; i++ ) {
+        let ans = t0+t1+t2;
+        t0 = t1;
+        t1 = t2;
+        t2 = ans;        
     }
+    
+    return t2
 };
