0380abface0745b4bbbfbce5481b755d8f36ad56
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-02 13:38:02 +0530

    Time: 129 ms (28.62%), Space: 45.2 MB (29.10%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var deleteAndEarn = function(nums) {
+    nums.sort((a, b) => a-b );
+    let cache = new Array(nums.length).fill(-1);
+    console.log(nums);
+    return helper(0);
+    
+    function helper(i) {
+        if( i >= nums.length )
+            return 0;
+        
+        if( cache[i] > -1 )
+            return cache[i];
+        
+        let num = nums[i];
+        let j = i, sum = 0;
+        while( num === nums[j] ) {
+            sum += nums[i];
+            j++;
+        }
+        let k = j;
+
+        num = num+1;
+        while( num === nums[j] ) {
+            j++;
+        }
+        let opt1 = helper(j) + sum;
+        let opt2 = helper(k);
+        // console.log(i, opt1, opt2)
+        cache[i] = Math.max(opt1, opt2)
+        return cache[i];
+    }
+    
+};
