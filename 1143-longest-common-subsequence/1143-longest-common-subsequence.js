a222c2364e66740b31d30cb099859aaea894d5c5
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-03 21:08:30 +0530

    Time: 123 ms (75.68%), Space: 53.1 MB (56.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,21 +4,24 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-  var sols = new Array(text1.length).fill(0).map(a => new Array(text2.length).fill(-1));
-    return helper( text1, text2, text1.length-1, text2.length-1, sols )
-};
-
-function helper( text1, text2, n, m, sols ) {
-    if( n < 0 || m < 0 )
-        return 0;
-
-    if (sols[n][m] !== -1) return sols[n][m];
-
-
-    if( text1.charAt(n) === text2.charAt(m) ){
-        return sols[n][m] = 1 + helper( text1, text2, n-1, m-1, sols );            
-    } else {
-        return sols[n][m] = Math.max( helper( text1, text2, n, m-1, sols ), 
-                                             helper( text1, text2, n-1, m, sols ) );
+    var sols = new Array(text1.length).fill(0).map(a => new Array(text2.length).fill(-1));
+    return helper( text1.length-1, text2.length-1 );
+    
+    function helper( i, j ) {
+        if( i < 0 || j < 0 )
+            return 0;
+        
+        if( sols[i][j] >-1 )
+            return sols[i][j];
+        
+        let ans;
+        if( text1.charAt(i) === text2.charAt(j) )
+            ans = 1 + helper( i-1, j-1 );
+        else 
+            ans = Math.max( helper(i-1, j), helper(i, j-1) );
+        
+        sols[i][j] = ans;
+        return ans;
     }
-}
+    
+};
