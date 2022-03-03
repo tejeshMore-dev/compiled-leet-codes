617319b7467828ddc667b01bf4379819214778f6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-03 22:34:39 +0530

    Time: 964 ms (30.11%), Space: 86.7 MB (35.18%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -3,6 +3,8 @@
  * @param {number[]} multipliers
  * @return {number}
  */
+
+// /* Top Down Approach
 var maximumScore = function(nums, multipliers) {
     let cache = new Array(multipliers.length).fill(0).map( a => new Array(multipliers.length).fill(0) );
     let length = nums.length;
@@ -22,3 +24,26 @@ var maximumScore = function(nums, multipliers) {
         return cache[i][l] = Math.max( opt1, opt2 );
     }
 };
+// */
+
+
+/* Bottom Up Apporach
+var maximumScore = function(nums, multipliers) {
+    let nLen = nums.length;
+    let mLen = multipliers.length;
+    
+    let dp = new Array(nLen+1).fill(0).map(a => new Array(mLen+1).fill(0));
+    
+    for( let m = mLen-1 ; m>=0; m-- ) {
+        for( let l = nLen-1 ; l>=0; l-- ) {
+            
+            let mul = multipliers[m];
+            let r = nLen - 1 - ( m -l );
+            
+            dp[m][l] = Math.max(mul * nums[l] + dp[m+1][l+1], mul * nums[r] + dp[m+1][l] )
+        }
+    }
+    
+    return dp[0][0];
+}
+*/
