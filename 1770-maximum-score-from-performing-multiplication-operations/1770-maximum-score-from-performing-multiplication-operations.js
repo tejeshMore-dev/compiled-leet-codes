4b4a7163ac9a2fc9ec7b14f471d3d8fd160b8440
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-03 20:48:31 +0530

    Time: 616 ms (62.41%), Space: 76.9 MB (78.79%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} multipliers
+ * @return {number}
+ */
+var maximumScore = function(nums, multipliers) {
+    let cache = new Array(multipliers.length).fill(0).map( a => new Array(multipliers.length).fill(0) );
+    let length = nums.length;
+    return helper( 0, 0, nums.length-1);
+    
+    function helper( i, l ) {
+        if( i === multipliers.length )
+            return 0;
+        
+        if( cache[i][l] )
+            return cache[i][l];
+        
+        let r = length - 1 - ( i - l );
+        
+        let opt1 = multipliers[i] * nums[l] + helper( i+1, l+1 );
+        let opt2 = multipliers[i] * nums[r] + helper( i+1, l );
+        // console.log(i, opt1, opt2)
+        return cache[i][l] = Math.max( opt1, opt2 );
+    }
+};
