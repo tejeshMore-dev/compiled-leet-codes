e38d467d2df832a3e91abc72fbf11cea111c976a
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-03 22:16:53 +0530

    Time: 199 ms (31.89%), Space: 60.8 MB (28.37%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -3,6 +3,8 @@
  * @param {string} text2
  * @return {number}
  */
+
+/* TOP DOWN APPROACH
 var longestCommonSubsequence = function(text1, text2) {
     var sols = new Array(text1.length+1).fill(0).map(a => new Array(text2.length).fill(-1));
     return helper( text1.length-1, text2.length-1 );
@@ -24,3 +26,26 @@ var longestCommonSubsequence = function(text1, text2) {
         return ans;
     }
 };
+*/
+
+// /* BOTTOM UP APPROACH
+var longestCommonSubsequence = function(text1, text2) {
+    let m = text1.length;
+    let n = text2.length;
+    let dp = new Array(m+1).fill(0).map( a => new Array(n+1).fill(0) );
+    
+    for( let i=1; i <= m; i++ ) {
+        for( let j=1; j <= n; j++ ) {
+            
+            if( text1[i-1] === text2[j-1] ) {
+                dp[i][j] = 1 + dp[i-1][j-1];                
+            }
+            else {
+                dp[i][j] = Math.max( dp[i-1][j], dp[i][j-1] );                
+            }
+        }
+    }
+    console.log(dp);
+    return dp[m][n];
+}
+// */
