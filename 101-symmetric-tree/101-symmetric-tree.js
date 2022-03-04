dff92be846592c9913555839b84b5be4f1a38d7e
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-04 20:28:53 +0530

    Time: 70 ms (92.72%), Space: 44.5 MB (41.21%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.js
+++ 101-symmetric-tree/101-symmetric-tree.js
@@ -22,9 +22,7 @@ var isSymmetric = function(root) {
     
         if( (node1 && !node2) || (!node1 && node2) )
             return false;
-        
-        // console.log(node1.val, node2.val);
-    
+            
         return ( node1.val === node2.val && 
                 helper( node1.left, node2.right ) &&
                 helper( node1.right, node2.left )
