15fabc80d3c07015663007ac416ae5e004120031
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-07 04:56:13 +0530

    Time: 86 ms (78.89%), Space: 44.8 MB (63.33%) - LeetHub

--- 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.js
+++ 1522-diameter-of-n-ary-tree/1522-diameter-of-n-ary-tree.js
@@ -15,27 +15,26 @@ var diameter = function(root) {
         return 0;
     
     let ans = 0;
-    traverse( root );
+    findDepth( root );
     return ans;
     
-    function traverse( node ) {
-        if( node.children.length === 0 ) {
+    function findDepth( node ) {
+        if( node.children.length === 0 )
             return 1;
-        } else {
-            let max1 = 0, max2 = 0;
-            
-            for( let child of node.children ) {
-                let d = traverse( child );
-                
-                if( d > max1 ) {
-                    max2 = max1;
-                    max1 = d;
-                } else if( d > max2 ) {
-                    max2 = d;
-                }
+        
+        let max1 = 0, max2 = 0;
+
+        for( let child of node.children ) {
+            let d = findDepth( child );
+
+            if( d > max1 ) {
+                max2 = max1;
+                max1 = d;
+            } else if( d > max2 ) {
+                max2 = d;
             }
-            ans = Math.max( ans, max1 + max2 );
-            return max1 + 1;
         }
+        ans = Math.max( ans, max1 + max2 );
+        return max1 + 1;
     }
 };
