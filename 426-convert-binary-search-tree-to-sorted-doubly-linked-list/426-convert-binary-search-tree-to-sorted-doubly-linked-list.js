b29e39bd1ddc99bdf4ee7f09779e9b838a70a3fb
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-08 15:27:23 +0530

    Time: 64 ms (95.16%), Space: 44.6 MB (7.27%) - LeetHub

---
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right) {
+ *      this.val = val;
+ *      this.left = left;
+ *      this.right = right;
+ *  };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var treeToDoublyList = function(root) {
+    if( !root )
+        return null;
+    
+    let prev = null, head = null;
+    createDL( root );
+    
+    head.left = prev;
+    prev.right = head;
+    return head;
+    
+    function createDL( node ) {
+        if( !node )
+            return null;
+        
+        createDL( node.left );
+        
+        if( !prev ) {
+            head = node;
+        } else {
+            prev.right = node;
+            node.left = prev;
+        }
+        
+        prev = node;
+        createDL( node.right );
+    }
+};
