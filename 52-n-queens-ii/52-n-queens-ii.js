b15b73bdd057199a284cd9335c026e7434a5296d
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-11 15:30:51 +0530

    Time: 85 ms (72.90%), Space: 44.1 MB (47.66%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function(n) {
+    let ans = 0;
+    let col = new Set();
+    let posDig = new Set();
+    let negDig = new Set();
+    
+    function backtrack(r ) {
+        if( r === n ) {
+            ans++;
+            return;
+        }
+        
+        for( let c=0; c<n; c++ ) {
+            if( col.has(c) || posDig.has(r+c) || negDig.has(r-c) )
+                continue;
+
+            col.add(c);
+            posDig.add(r+c);
+            negDig.add(r-c);
+
+            backtrack(r+1);
+
+            col.delete(c);
+            posDig.delete(r+c);
+            negDig.delete(r-c);
+        }
+    }
+    
+    backtrack(0);
+    return ans;
+    
+};
