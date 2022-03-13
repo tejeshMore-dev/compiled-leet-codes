ed0a3d2e6cb0e0b283c54795cdad5a937911997e
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-13 20:27:57 +0530

    Time: 6842 ms (6.15%), Space: 149.5 MB (9.23%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function(strs, m, n) {
+    let cache = {};
+    return helper( 0, m , n );
+    
+    function helper( i, zeroes, ones ) {
+        if( i === strs.length )
+            return 0;
+        
+        if( cache.hasOwnProperty(`${i}-${zeroes}-${ones}`) )
+            return cache[`${i}-${zeroes}-${ones}`];
+        
+        let [ z, o ] = count(strs[i]);
+        let opt_taken = -1;
+        if(  zeroes - z >= 0 && ones - o >= 0 ) {
+            opt_taken = helper( i+1, zeroes-z, ones-o ) + 1;
+        }
+        let opt_not_taken = helper( i+1, zeroes, ones )
+        
+        cache[`${i}-${zeroes}-${ones}`] = Math.max( opt_taken, opt_not_taken ); 
+        return cache[`${i}-${zeroes}-${ones}`];
+    }
+    
+    function count( str ) {
+        let ans = [ 0, 0 ];
+        
+        for( let i=0; i < str.length; i++ ) {
+           if( str[i] === "0" )
+               ans[0]++;
+            else
+                ans[1]++
+        }
+        
+        return ans;
+    }
+};
