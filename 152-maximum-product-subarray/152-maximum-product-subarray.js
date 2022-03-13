817cb17433f0b6c8f5a9a0329dfc165b34f90fae
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-13 18:55:18 +0530

    Time: 112 ms (30.04%), Space: 42.4 MB (59.32%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.js
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -3,11 +3,18 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let currMin = nums[0], currMax = nums[0];
+    let currMin = 1, currMax = 1;
     let res = nums[0];
     
-    for( let i=1; i<nums.length; i++ ) {
+    for( let i=0; i<nums.length; i++ ) {        
         let num = nums[i];
+        
+        if( num === 0 ) {
+            currMin = 1;
+            currMax = 1;
+            res = Math.max( res, num );
+            continue;
+        }
         
         let temp = Math.max( num, num * currMax, num * currMin );
         currMin = Math.min( num, num * currMax, num * currMin );
