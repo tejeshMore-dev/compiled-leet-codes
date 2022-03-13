b4347c8b92923f9d946f10897fe145710f940b5f
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-13 07:19:33 +0530

    Time: 58 ms (94.35%), Space: 42 MB (45.71%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -2,8 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+/* bottom up O(N) O(1)
 var rob = function(nums) {
-    let cache = {};
+    let rob1 = 0;
+    let rob2 = 0;
+    
+    for( let num of nums ) {
+        let max = Math.max( num + rob1, rob2 );
+        rob1 = rob2;
+        rob2 = max;
+    }
+    
+    return rob2;
+}
+*/
+
+// /* top down O(N) O(N)
+var rob = function(nums) {
+    let cache = new Array(nums.length).fill(-1);
     
     return helper(0);
     
@@ -11,11 +28,12 @@ var rob = function(nums) {
         if( i >= nums.length )
             return 0;
 
-        if( cache.hasOwnProperty(i) )
+        if( cache[i] > -1 )
             return cache[i];
         
         let ans = Math.max( helper(i+1), helper(i+2) + nums[i] );
         cache[i] = ans;
         return ans;
     }
-}      
+}
+// */
