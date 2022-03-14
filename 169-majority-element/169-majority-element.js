ce2f2e11c6721b80cf103152b544b87e8dbd7c01
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-14 19:11:27 +0530

    Time: 68 ms (91.59%), Space: 44 MB (49.76%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let result;
+    let candidate = 0;
     let count = 0;
     
-    for ( let i=0; i<nums.length; i++ ) {
-        if( result !== nums[i] && count === 0  ) {
-            result = nums[i];
+    for ( let num of nums) {
+        if( count === 0 )
+            candidate = num;
+        
+        if( candidate === num )
             count++;
-        } else if( result === nums[i] ) {
-           count++; 
-        }  else {
+        else
             count--;
-        }
     }
     
-    return result;
+    return candidate;
 };
