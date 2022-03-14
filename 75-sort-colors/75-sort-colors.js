4c43a4ebf39daf8b426f1ee9f4ac3f293d6a51db
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-14 09:35:18 +0530

    Time: 115 ms (16.28%), Space: 41.9 MB (61.75%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -2,6 +2,33 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// /* Approach 2
+var sortColors = function(nums) {
+    let lp = 0, mid = 0, rp = nums.length-1;
+    
+    while( mid <= rp ) {
+        if( nums[mid] === 0 ) {
+            swap(lp, mid);
+            lp++;
+            mid++;
+        } else if( nums[mid] === 2 ) {
+            swap(rp, mid);
+            rp--;
+        } else {
+            mid++;            
+        }
+    }
+    
+    function swap( p1, p2 ) {
+        let temp = nums[p1];
+        nums[p1] = nums[p2];
+        nums[p2] = tamp;
+    }
+    
+};
+// */
+
+// /* Approach 1
 var sortColors = function(nums) {
     let map = { 0:0, 1:0, 2:0 }
     
@@ -17,5 +44,5 @@ var sortColors = function(nums) {
             count--;
         }
     }
-    
 };
+// */
