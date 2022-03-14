f15577de72cbadaff8277a072f219daaa87c2fc2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-14 11:12:18 +0530

    Time: 102 ms (29.38%), Space: 42.1 MB (49.76%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    let l = 0, r = matrix.length-1;
+    
+    /*
+    
+     l     top                  r    
+    
+    
+    bottom
+    
+    */
+    while( l < r ) {
+        let top = l, bottom = r;
+        
+        for( let i=0; i < r-l; i++ ) {
+            let temp = matrix[top][l+i];
+        
+            matrix[top][l+i] = matrix[bottom-i][l];
+            
+            matrix[bottom-i][l] = matrix[bottom][r-i];
+            
+            matrix[bottom][r-i] = matrix[top+i][r];
+            
+            matrix[top+i][r] = temp ;
+        }
+        l++;
+        r--;
+    }
+};
