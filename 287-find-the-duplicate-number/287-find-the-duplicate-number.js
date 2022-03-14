f174fafee533e4935a0b2fd4cf8547e401fd1601
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-14 12:50:52 +0530

    Time: 111 ms (74.24%), Space: 49.9 MB (69.63%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 
-// var findDuplicate = function(nums) {
-//     let sp = nums[0], fp = nums[nums[0]];
+var findDuplicate = function(nums) {
+    let sp = nums[0], fp = nums[nums[0]];
     
-//     while( sp !== fp ) {
-//         sp = nums[sp];
-//         fp = nums[nums[fp]];
-//     }
+    while( sp !== fp ) {
+        sp = nums[sp];
+        fp = nums[nums[fp]];
+    }
     
-//     fp = 0;
-//      while( sp !== fp ) {
-//         sp = nums[sp];
-//         fp = nums[fp];
-//     }
+    fp = 0;
+     while( sp !== fp ) {
+        sp = nums[sp];
+        fp = nums[fp];
+    }
     
-//     return sp;
-// };
+    return sp;
+};
 
 var findDuplicate = function(nums) {
     let duplicate = -1;
