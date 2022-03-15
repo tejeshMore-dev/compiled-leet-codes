b65c238d05af94422dbeb11b795d6cacc8e951f2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-15 22:43:05 +0530

    Time: 132 ms (32.57%), Space: 44.3 MB (30.29%) - LeetHub

--- 259-3sum-smaller/259-3sum-smaller.js
+++ 259-3sum-smaller/259-3sum-smaller.js
@@ -11,17 +11,17 @@ var threeSumSmaller = function(nums, target) {
     nums.sort((a, b) =>  a-b );
     
     for( let i = 0; i < nums.length-2; i++ ) {
-            let lp = i+1, rp = nums.length-1;
-            
-            while( lp < rp ) {
-                let currSum = nums[lp] + nums[rp] + nums[i];
-                if( currSum < target ) {
-                    res += rp - lp;
-                    lp++;
-                }  else {
-                    rp--;
-                }
+        let lp = i+1, rp = nums.length-1;
+
+        while( lp < rp ) {
+            let currSum = nums[lp] + nums[rp] + nums[i];
+            if( currSum < target ) {
+                res += rp - lp;
+                lp++;
+            }  else {
+                rp--;
             }
+        }
     }
     
     return res;
