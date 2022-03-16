0ec3b2a10c841f663795cc91ce6fd19859dc8ab1
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-16 21:09:31 +0530

    Time: 64 ms (93.38%), Space: 44.9 MB (64.30%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.js
@@ -3,11 +3,31 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
-    let ans = 0;
+    let ans = -1;
+    if( nums.length == 1 )
+        return nums[0]
     
-    for( let num of nums ) {
-        ans ^= num;
+    let lp = 0, rp = nums.length-1;
+    if( nums[0] !== nums[1] ) {
+        return nums[0];
+    } else if( nums[rp] !== nums[rp-1] ) {
+        return nums[rp];        
     }
     
+    while( lp <= rp ) {
+        let mid = Math.floor( lp + (rp-lp)/2 );
+        
+        if( nums[mid] !== nums[mid+1] && nums[mid] !== nums[mid-1] )
+            return ans = nums[mid];
+        
+        if( ( mid%2 === 0 && nums[mid] === nums[mid+1] ) ||
+            ( mid%2 === 1 && nums[mid] === nums[mid-1] ) ) {
+            lp = mid+1
+        } else {
+            rp = mid-1;
+        }
+    }
+    
+
     return ans;
 }
