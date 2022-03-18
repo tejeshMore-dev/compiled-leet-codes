d5fc1176167f9d94add8ba2ee7733baed41dd562
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-18 07:34:48 +0530

    Time: 198 ms (73.01%), Space: 88.7 MB (20.25%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -6,7 +6,6 @@
 // /* Top Down Approach
 var canPartition = function(nums) {
     let sum = 0;
-    
     for( let num of nums ) {
         sum += num;
     }
@@ -16,24 +15,47 @@ var canPartition = function(nums) {
     
     let subsetSum = sum/2;
     let cache = new Array(nums.length).fill(-1).map(a => new Array(subsetSum+1).fill(-1) );
-    return findSubsetSum( 0, subsetSum, cache );
+    return findSubsetSum( nums.length-1, subsetSum, cache );
     
     function findSubsetSum( i, s, cache ) {
         if( s === 0 )
             return true
-        else if( i === nums.length || s < 0 )
+        else if( i === 0 || s < 0 )
             return false
         
         if( cache[i][s] !== -1 )
             return cache[i][s];
         
-        return cache[i][s] = findSubsetSum( i+1, s-nums[i], cache ) || findSubsetSum( i+1, s, cache );
+        return cache[i][s] = findSubsetSum( i-1, s-nums[i], cache ) || findSubsetSum( i-1, s, cache );
     }
 };
 // */
 
 /* Bottom up Approach
 var canPartition = function(nums) {
-
+    let sum = 0;
+    for( let num of nums ) {
+        sum += num;
+    }
+    
+    if( sum%2 !== 0 )
+        return false;
+    
+    let subsetSum = sum/2;
+    let dp = new Array(nums.length+1).fill(0).map(a => new Array(subsetSum+1).fill() );
+    
+    for( let i=0; i <= nums.length; i++ ) {
+        for( let s=0; s <= subsetSum; s++  ) {
+            
+            if( s === 0 || i === 0 )
+                dp[i][s] = false;
+            else if ( nums[i-1] )
+                dp[i][s] = false;
+            
+            dp[i][s] = dp[i+1][s-nums[i]] || dp[i+1][s]
+        }
+    }
+    
+    return dp[nums.length-1][subsetSum];
 };
 */
