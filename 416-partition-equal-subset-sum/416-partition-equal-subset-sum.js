ed3c4c73082af4bb6738b049fcbdd2dc0df59d20
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-18 09:02:30 +0530

    Time: 374 ms (38.19%), Space: 89.1 MB (15.64%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 
-// /* Top Down Approach
+/* Top Down Approach
 var canPartition = function(nums) {
     let sum = 0;
     for( let num of nums ) {
@@ -15,23 +15,23 @@ var canPartition = function(nums) {
     
     let subsetSum = sum/2;
     let cache = new Array(nums.length).fill(-1).map(a => new Array(subsetSum+1).fill(-1) );
-    return findSubsetSum( nums.length-1, subsetSum, cache );
+    return findSubsetSum( 0, subsetSum, cache );
     
     function findSubsetSum( i, s, cache ) {
         if( s === 0 )
             return true
-        else if( i === 0 || s < 0 )
+        else if( i === nums.length || s < 0 )
             return false
         
         if( cache[i][s] !== -1 )
             return cache[i][s];
         
-        return cache[i][s] = findSubsetSum( i-1, s-nums[i], cache ) || findSubsetSum( i-1, s, cache );
+        return cache[i][s] = findSubsetSum( i+1, s-nums[i], cache ) || findSubsetSum( i+1, s, cache );
     }
 };
-// */
+*/
 
-/* Bottom up Approach
+// /* Bottom up Approach
 var canPartition = function(nums) {
     let sum = 0;
     for( let num of nums ) {
@@ -42,20 +42,22 @@ var canPartition = function(nums) {
         return false;
     
     let subsetSum = sum/2;
-    let dp = new Array(nums.length+1).fill(0).map(a => new Array(subsetSum+1).fill() );
+    let dp = new Array(nums.length+1).fill(0).map(a => new Array(subsetSum+1).fill(0) );
     
     for( let i=0; i <= nums.length; i++ ) {
         for( let s=0; s <= subsetSum; s++  ) {
             
-            if( s === 0 || i === 0 )
+            if( s === 0 )
+                dp[i][s] = true;
+            else if ( i === 0 )
                 dp[i][s] = false;
-            else if ( nums[i-1] )
-                dp[i][s] = false;
-            
-            dp[i][s] = dp[i+1][s-nums[i]] || dp[i+1][s]
+            else if ( nums[i-1] === s )
+                dp[i][s] = true;
+            else
+             dp[i][s] = dp[i-1][s-nums[i-1]] || dp[i-1][s]
         }
     }
     
-    return dp[nums.length-1][subsetSum];
+    return dp[nums.length][subsetSum];
 };
-*/
+// */
