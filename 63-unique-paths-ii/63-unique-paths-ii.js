386f26c2960022367f0f7ae5f73fe5a12a633b50
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-19 11:22:29 +0530

    Time: 79 ms (68.74%), Space: 42.5 MB (67.15%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.js
+++ 63-unique-paths-ii/63-unique-paths-ii.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 
-// /* Top Down Approach
+/* Top Down Approach
 var uniquePathsWithObstacles = function(obstacleGrid) {
     let rows = obstacleGrid.length;
     let cols = obstacleGrid[0].length;
@@ -27,9 +27,33 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
         return cache[r][c] = opt1+opt2;
     }
 };
-// */
-
-/* Top Down Approach
-var uniquePathsWithObstacles = function(obstacleGrid) { 
-}
 */
+
+// /* Top Down Approach
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    if( obstacleGrid[0][0] === 1 )
+        return 0;
+    
+    let rows = obstacleGrid.length;
+    let cols = obstacleGrid[0].length;
+    
+    let dp = new Array(rows).fill(1).map( a => new Array( cols ).fill(1) );
+    
+    for( let r=0; r < rows; r++ ) {
+        for( let c=0; c < cols; c++ ) {
+            if( r === 0 && c === 0 )
+                dp[r][c] = 1
+            else if( obstacleGrid[r][c] === 1 )
+                dp[r][c] = 0;
+            else if( r === 0 )
+                dp[r][c] = dp[r][c-1] === 0 ? 0 : dp[r][c-1];
+            else if( c === 0 )
+                dp[r][c] = dp[r-1][c] === 0 ? 0 : dp[r-1][c];
+            else 
+                dp[r][c] = dp[r-1][c] + dp[r][c-1] 
+        }
+    }
+    
+    return dp[rows-1][cols-1];
+}
+// */
