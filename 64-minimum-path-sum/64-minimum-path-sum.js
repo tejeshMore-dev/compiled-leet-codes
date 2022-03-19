fa14f29efc2ea0a9a188484d03182f7b8638b3c2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-19 09:58:27 +0530

    Time: 123 ms (24.48%), Space: 45.3 MB (22.85%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 
-// /* Top down Approach
+/* Top down Approach
 var minPathSum = function(grid) {
     let rows = grid.length;
     let cols = grid[0].length;
@@ -21,5 +21,29 @@ var minPathSum = function(grid) {
         
         return cache[r][c] = grid[r][c] + Math.min( helper(r+1, c), helper(r, c+1) )
     }
+};
+*/
+
+// /* Bottom Up Approach
+var minPathSum = function(grid) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+    
+    let dp = new Array(rows).fill(1).map( a => new Array(cols).fill(1) );
+    
+    for( let r=0; r < rows; r++ ) {
+        for( let c=0; c < cols; c++ ) {
+            if( r === 0 && c === 0 )
+                dp[r][c] = grid[r][c];
+            else if( r === 0 )
+                dp[r][c] = grid[r][c] + dp[r][c-1];
+            else if( c === 0 )
+                dp[r][c] = grid[r][c] + dp[r-1][c];
+            else
+                dp[r][c] = Math.min(dp[r-1][c], dp[r][c-1]) + grid[r][c];
+        }
+    }
+    
+    return dp[rows-1][cols-1];
 };
 // */
