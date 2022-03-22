5cd491186809045052a5eac75fe8243fc398d99f
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-22 09:57:29 +0530

    Time: 863 ms (13.67%), Space: 129.5 MB (28.06%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var minReorder = function(n, connections) {
+    let edges = new Set();
+    
+    for( let connection of connections ) {
+        let [ s, d ] = connection;
+        edges.add( `${s}-${d}` );
+    }
+    
+    const cityGraph = {};
+    for( let connection of connections ) {
+        let [ s, d ] = connection;
+        if( !cityGraph[s] )
+            cityGraph[s] = [];
+        
+        if( !cityGraph[d] )
+            cityGraph[d] = [];
+        
+        cityGraph[s].push(d);
+        cityGraph[d].push(s);
+    }
+    
+    let changes = 0;
+    dfs(0, new Set())
+    return changes;
+    
+    function dfs(city, visited) {
+        visited.add(city);
+        for( let nCity of cityGraph[city] ) {
+            if( visited.has(nCity) )
+                continue;
+            
+            if( !edges.has(`${nCity}-${city}`) )
+                changes++
+            dfs( nCity, visited )
+        }
+    }
+};
