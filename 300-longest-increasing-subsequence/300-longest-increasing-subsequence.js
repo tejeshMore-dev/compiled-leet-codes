b9f173526b0bf2fd39bae08214b68cdb8ee3c0f6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-23 06:02:45 +0530

    Time: 160 ms (72.91%), Space: 44 MB (51.19%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -4,18 +4,15 @@
  */
 var lengthOfLIS = function(nums) {
     let dp = new Array(nums.length).fill(1);
-    let res = 1;
     
-    for( let i=1; i < nums.length; i++ ){
+    for( let i=1; i < nums.length; i++ ) {
         for( let j=0; j < i; j++ ) {
-            if( nums[i] > nums[j] ) {
-                dp[i] = Math.max(dp[i], dp[j]+1);                
-            }
+            if( nums[i] > nums[j] )
+                dp[i] = Math.max( 1+dp[j], dp[i] )
         }
-        res = Math.max(res, dp[i]);
     }
     
-    return res;
+    return Math.max( ...dp )
 };
 
 /*
