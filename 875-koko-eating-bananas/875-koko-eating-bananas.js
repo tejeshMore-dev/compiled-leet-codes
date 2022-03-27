3780b7a3359dcca01553512e7d70e051d054dc0f
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-27 17:44:55 +0530

    Time: 155 ms (22.37%), Space: 48.7 MB (5.42%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let lp = 1, rp = Math.max( ...piles );
+    let res = Number.MAX_VALUE;
+    
+    while( lp <= rp ) {
+        let mid = Math.floor( lp + (rp-lp)/2 );
+        let hrReq = findHrs(mid);
+        console.log(mid, hrReq)
+        
+        if( hrReq > h ) {
+            lp = mid+1;
+        } else {
+            res = Math.min(res, mid);
+            rp = mid-1;
+        }
+    }
+    
+    return res;
+    
+    function findHrs(val) {
+        let ans = 0;
+        
+        for( let pile of piles ) {
+            ans += Math.ceil(pile/val);
+        }
+        
+        return ans;
+    }
+};
