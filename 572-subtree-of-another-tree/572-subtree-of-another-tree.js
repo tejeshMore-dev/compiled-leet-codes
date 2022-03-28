f913b6cf1d265b5473fef98fda87f33f058372f6
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-28 08:17:20 +0530

    Time: 84 ms (91.59%), Space: 49.2 MB (70.78%) - LeetHub

---
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if( !subRoot )
+        return true
+    
+    if( !root )
+        return false;
+    
+    if( sameTree(root, subRoot) )
+        return true;
+    
+    return ( isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot) );
+    
+    function sameTree( r, s ) {
+        if( !s && !r )
+            return true
+        
+        if( s && r && s.val === r.val && sameTree(r.left, s.left) && sameTree(r.right, s.right) )
+            return true
+        
+        return false;
+    }
+};
