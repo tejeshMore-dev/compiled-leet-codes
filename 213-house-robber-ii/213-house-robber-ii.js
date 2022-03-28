ae9d508f7463d5e627163f0a2ae85cbdf0b5e7e7
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-28 17:31:13 +0530

    Time: 99 ms (22.31%), Space: 42.1 MB (60.02%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {    
+    let temp = nums.shift();
+    let opt1 = helper(nums);
+    
+    nums.unshift(temp);
+    temp = nums.pop();
+    let opt2 = helper(nums);
+    nums.push(temp);
+    return Math.max( nums[0], opt1, opt2 );
+        
+    function helper( arr ) {
+        let rob1 = 0;
+        let rob2 = 0;
+
+        for( let i=0; i <arr.length; i++ ) {
+            let temp = Math.max( rob2, arr[i] + rob1 );
+            rob1 = rob2;
+            rob2 = temp
+        }
+        return rob2;
+    }
+};
