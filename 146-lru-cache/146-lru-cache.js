9f2741a162098d043066c3cbc38b99fd75b7e7d7
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-28 15:21:25 +0530

    Time: 879 ms (5.07%), Space: 126.7 MB (25.60%) - LeetHub

--- 146-lru-cache/146-lru-cache.js
+++ 146-lru-cache/146-lru-cache.js
@@ -2,47 +2,29 @@
  * @param {number} capacity
  */
 
-var _map;
-var _capacity;
-
 var LRUCache = function(capacity) {
-    _capacity = capacity;
-    _map = new Map();
+    this.capacity = capacity;
+    this.map = new Map();
 };
 
-/** 
- * @param {number} key
- * @return {number}
- */
+
 LRUCache.prototype.get = function(key) {
-    if( _map.has(key) ) {
-        let temp = _map.get(key);
-        _map.delete(key);
-        _map.set(key, temp);
+    if( this.map.has(key) ) {
+        let temp = this.map.get(key);
+        this.map.delete(key);
+        this.map.set(key, temp);
         return temp;
     }
     
     return -1;
 };
 
-/** 
- * @param {number} key 
- * @param {number} value
- * @return {void}
- */
 LRUCache.prototype.put = function(key, value) {
     
-    if( _map.has(key) ) {
-        _map.delete(key);
-    } else if( _capacity === _map.size  ) {
-        _map.delete(_map.keys().next().value)              
+    if( this.map.has(key) ) {
+        this.map.delete(key);
+    } else if( this.capacity === this.map.size  ) {
+        this.map.delete(this.map.keys().next().value)              
     }
-    _map.set(key, value);
+    this.map.set(key, value);
 };
-
-/** 
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
