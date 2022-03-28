594ec1e55bc802e0f0764d392cdcfe5d396b93fa
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-28 11:42:02 +0530

    Time: 72 ms (82.45%), Space: 44.6 MB (27.02%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var restoreIpAddresses = function(s) {
+    let ans = [];
+    const MAX = 256;
+    const TOTAL_DOTS = 4;
+    const MAX_DIGIT = 3;
+    backtrack(0, 0, []);
+    return ans;
+    
+    function backtrack( i, dots, combination ) {
+        if( i === s.length && dots === TOTAL_DOTS ){
+            combination.pop();
+            ans.push( combination.join("") );
+            return;
+        }
+        
+        if( i >= s.length || dots >= TOTAL_DOTS  )
+            return;
+        
+        
+        for( let j=i; j < Math.min(i+MAX_DIGIT, s.length); j++ ) {
+            let val = parseInt( s.substring(i, j+1));
+            if( val < MAX && ( s[i] !== "0" || i === j ) )
+                backtrack(j+1, dots+1, [ ...combination, val, "." ] )
+        }
+    }
+};
