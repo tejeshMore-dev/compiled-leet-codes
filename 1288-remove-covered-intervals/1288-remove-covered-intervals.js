edeb4cf9efed6f059eaeac760693d47f9e25f2ba
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-03-31 10:22:04 +0530

    Time: 90 ms (64.40%), Space: 44.3 MB (82.37%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var removeCoveredIntervals = function(intervals) {
+    let res = 0;
+    let current = [ -1, -1 ];
+    
+    intervals.sort((a, b) => a[0] === b[0] ? b[1] - a[1] : a[0] - b[0] );
+    for( let i=0; i < intervals.length; i++ ) {
+        if( intervals[i][1] > current[1] ) {
+            current = intervals[i];
+            res++;
+        }
+    }
+    
+    return res;
+};
