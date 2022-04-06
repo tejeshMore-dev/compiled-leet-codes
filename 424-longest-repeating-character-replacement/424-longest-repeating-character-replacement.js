571a924231d5e3a67a80d46c6689532958d19f9d
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-04-06 19:46:06 +0530

    Time: 751 ms (7.19%), Space: 53.3 MB (5.34%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let lp = 0;
+    let map = {};
+    let res = 0;
+
+    for( let rp in s ) {
+        let char  = s[rp];
+        if(  !map[char] )
+            map[char] = 0;
+        
+        map[char] += 1;
+        
+        while( rp-lp+1 - Math.max( ...Object.values(map) )  > k ) {
+            map[s[lp]] -= 1;
+            lp++;
+        }
+                
+       res = Math.max( res, rp-lp+1 ); 
+    }
+    
+    return res;
+};
