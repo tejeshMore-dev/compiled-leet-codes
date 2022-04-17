9bafcbf8197c6565f3957fd84aac2fba19e78758
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-04-18 04:39:40 +0530

    Time: 71 ms (78.30%), Space: 42.9 MB (87.17%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -3,34 +3,22 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    let subset = [], result = [];
+    let set = [], result = [];
+    findSubSets( 0 );
+    return result;
     
-    function helper( i ) {
+    function findSubSets( i ) {
         if( i === nums.length ) {
-            result.push( [ ...subset ] );
-            return
+            result.push( [ ...set ] );
+            return;
         }
         
-        subset.push( nums[i] );
-        helper( i+1 );
-        subset.pop();
-        
-        helper( i+1 );
+        set.push( nums[i] );
+        findSubSets( i+1 );
+        set.pop();
+        findSubSets( i+1 );
     }
-    
-    helper( 0 );
-    return result;
 };
-
-/*
-two choices we can have either use ith value or don't use it.
-decision tree will look like below
-
-[] [1] 
-[] [2] [1] [12]
-[] [3] [2] [23] [1] [13] [12] [123] 
-
-*/
 
 
 
