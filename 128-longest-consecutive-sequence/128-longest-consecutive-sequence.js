a0f4b71db145db3d0f217bab6d7ace75e969d572
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-04-23 13:08:25 +0530

    Time: 527 ms (35.28%), Space: 50.9 MB (43.31%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if(nums.length === 0) return 0
+    if( nums.length === 0 )
+        return 0
+    
     let set = new Set(), ans = 1;
-    
-    for( let i = 0; i < nums.length; i++ ) {
-        set.add(nums[i]);
-    }
-  
-    for( let i = 0; i < nums.length; i++ ) {
-       if( !set.has(nums[i] -1) && set.has(nums[i]+1)  ) {
-           let count = 1, val = nums[i];
-           while( set.has(++val) ) {
-               count++;
-           }
-           
-           ans = Math.max(ans, count);
-       }
+    for( let num of nums ) {
+        set.add(num);
     }
     
-    return ans
+    for( let num of nums ) {
+        if( !set.has(num-1) && set.has(num+1) ) {
+            let count = 1, val = num;
+            while( set.has(++val) ) {
+                count++;
+            }
+            
+            ans = Math.max( ans, count );
+        }
+    }
+    
+    return ans;
 };
