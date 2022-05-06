81f0f210303428042f397259c37a28dc7f6384eb
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-05-06 10:59:58 +0530

    Time: 137 ms (56.32%), Space: 47.5 MB (49.54%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -18,11 +18,11 @@ var addTwoNumbers = function(l1, l2) {
         let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + c;
         
         let node = new ListNode(sum % 10);
+        c = Math.floor(sum/10);
+        
         pointer.next = node;
         pointer = node;
 
-        c = Math.floor(sum/10);
-        
         l1 = l1 ? l1.next : l1;
         l2 = l2 ? l2.next : l2;
     }
