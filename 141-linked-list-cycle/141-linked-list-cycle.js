3da80645b46c639a4a1d7fabc5a746195b2e45ce
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-05-30 10:17:19 +0530

    Time: 137 ms (9.53%), Space: 44.8 MB (75.60%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -13,13 +13,14 @@
 var hasCycle = function(head) {
     let sp = head, fp = head, result = false;
     
-    while( sp && sp.next && fp.next && fp.next.next ) {
+    while( sp && sp.next && fp && fp.next ) {
         sp = sp.next;
         fp = fp.next.next;
-          
+        
+        
         if( sp === fp )
             return result = true;
     }
     
-    return result;
+    return false;
 };
