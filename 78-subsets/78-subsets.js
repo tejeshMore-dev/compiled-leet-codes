c43652082621971a40ac3677d8acdcdda69cf254
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-05-30 08:48:01 +0530

    Time: 133 ms (5.30%), Space: 42.8 MB (94.47%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -4,19 +4,20 @@
  */
 var subsets = function(nums) {
     let ans = [], temp = [];
-    findSubSet( 0 );
+    findSubSet(0);
     return ans;
     
     function findSubSet( i ) {
         if( i === nums.length ) {
-            ans.push( [ ...temp ] )
-            return;
+            ans.push( [ ...temp ] );
+            return;            
         }
         
         temp.push( nums[i] );
-        findSubSet( i+1 );
+        findSubSet(i+1);
         
         temp.pop();
-        findSubSet( i+1 );
+        findSubSet(i+1);
     }
+    
 };
