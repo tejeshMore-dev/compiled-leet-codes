a77467919b7ccd72f6939325eb7fa3dbfafc965d
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-06-01 10:38:18 +0530

    Time: 91 ms (41.16%), Space: 45.1 MB (47.21%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,15 +4,17 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let lp = 0, rp = nums.length-1, result = -1;
+    let lp = 0, rp = nums.length-1;
+    let result = -1;
     
     while( lp <= rp ) {
-        let mid = Math.floor( lp + (rp-lp)/2 );
+        let mid = lp + Math.floor((rp-lp)/2 );
         
         if( nums[mid] === target )
             return result = mid;
+        
         if( nums[mid] < target )
-            lp = mid+1;
+            lp = mid+1;    
         else
             rp = mid-1;
     }
