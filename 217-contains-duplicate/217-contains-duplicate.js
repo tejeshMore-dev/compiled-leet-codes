5133747ae711b387267d9f3bca286848afd22433
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-19 10:55:46 +0530

    Time: 384 ms (15.68%), Space: 50.9 MB (48.78%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,13 +3,31 @@
  * @return {boolean}
  */
 
-// /* Hashset Equal
+
+// /* sort
+var containsDuplicate = function(nums) { // TC O(N) | SC O(N)
+    nums.sort((a, b) => a-b );
+    
+    return hasDuplicate(nums);
+};
+
+const hasDuplicate = function (nums){
+        for( let i=0; i < nums.length-1; i++ ) {
+            if( nums[i] === nums[i+1] )
+                return true
+        }
+        
+        return false;
+    }
+// */
+
+/* Hashset Equal
 var containsDuplicate = function(nums) { // TC O(N) | SC O(N)
     let set = new Set(nums);
     
     return set.size !== nums.length
 };
-// */
+*/
 
 /* Early Exit
 var containsDuplicate = function(nums) { // TC O(N) | SC O(N)
