cb552defbadb37b019e6ccc133a618602c03f0e2
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-21 21:13:46 +0530

    Time: 146 ms (46.78%), Space: 50.7 MB (68.96%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,44 +2,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-
-
-// /* sort
-var containsDuplicate = function(nums) { // TC O(N) | SC O(N)
-    nums.sort((a, b) => a-b );
-    
-    return hasDuplicate(nums);
-};
-
-const hasDuplicate = function (nums){
-        for( let i=0; i < nums.length-1; i++ ) {
-            if( nums[i] === nums[i+1] )
-                return true
-        }
-        
-        return false;
-    }
-// */
-
-/* Hashset Equal
-var containsDuplicate = function(nums) { // TC O(N) | SC O(N)
-    let set = new Set(nums);
-    
-    return set.size !== nums.length
-};
-*/
-
-/* Early Exit
-var containsDuplicate = function(nums) { // TC O(N) | SC O(N)
+var containsDuplicate = function(nums) { // TC:O(N) SC:O(N)
     let set = new Set();
     
-    for( let num of nums ) {
+    for (let num of nums) {
         if( set.has(num) )
-            return true;
+            return true
         
         set.add(num);
     }
     
     return false;
 };
-*/
