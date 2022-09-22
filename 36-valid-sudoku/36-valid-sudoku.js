fc875c283879ccb8e9528822f576e24af8fa9035
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-22 22:42:33 +0530

    Time: 152 ms (20.85%), Space: 45.4 MB (51.76%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,41 +3,39 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    let row = { };
+    let row = {};
     let col = {};
     let subGrid = {};
     
     for( let r=0; r<9; r++ ) {
         for( let c=0; c<9; c++ ) {
-            if( board[r][c] !== "." ) {
-                let val = board[r][c];
-                if( has(row, r, val) || has(col, c, val) || has(subGrid, `${Math.floor(r/3)}-${Math.floor(c/3)}`, val) )
+            let val = board[r][c];
+            if(val !== "." ){
+                let subGridkey = `${Math.floor(r/3)}-${Math.floor(c/3)}`;
+                if( has(row, r, val) || has(col, c, val) || has(subGrid, subGridkey, val) ) {
                     return false;
-                
+                }
+
                 add(row, r, val);
                 add(col, c, val);
-                add(subGrid, `${Math.floor(r/3)}-${Math.floor(c/3)}`, val);
+                add(subGrid, subGridkey, val); 
             }
-            
         }
     }
     
     return true;
 };
 
-function add( map, key, val ) {
-    if( !map[key] ) {
+const add = function( map, key, val ) {
+    if( !map[key] )
         map[key] = new Set();
-        map[key].add(val);
-    } else {
-        map[key].add(val);
-    }
+    
+    map[key].add(val);
 }
 
-
-function has( map, key, val ) {
-    if( !map[key] )
+const has = function( map, key, val ){
+    if(!map.hasOwnProperty(key))
         return false;
     
-    return map[key].has(val);
+    return map[key].has(val);   
 }
