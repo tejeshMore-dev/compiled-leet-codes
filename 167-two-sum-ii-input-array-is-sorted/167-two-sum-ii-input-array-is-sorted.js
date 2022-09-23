e6761080867656207fd56dc16712465689243445
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-23 23:24:50 +0530

    Time: 69 ms (91.84%), Space: 43.2 MB (33.22%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,17 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let lp = 0;
-    let rp = numbers.length-1;
+    let lp = 0, rp = numbers.length-1;
+    let result = [ -1, -1];
     
-    while( lp<rp ) {
-        let sum = numbers[rp] + numbers[lp];
-        
+    while( lp <  rp) {
+        let sum = numbers[lp] + numbers[rp];
         if( sum === target )
-            return [lp+1,rp+1]
-        else if ( sum > target )
-            rp--
+            return result = [ lp+1, rp+1 ];
+        else if( sum < target )
+            lp++;
         else
-            lp++
+            rp--;
     }
+    
+    return result;
 };
