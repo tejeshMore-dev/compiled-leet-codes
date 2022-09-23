fd8f08c14c54d62d42ec1e570b126364367aa6b1
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-24 00:05:37 +0530

    Time: 127 ms (41.25%), Space: 49.6 MB (58.43%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let res = 0;
+    let lp = 0, rp = height.length-1;
+    
+    while( lp < rp ) {
+        let area = (rp-lp) * Math.min(height[lp], height[rp]);
+        // console.log(lp, rp, area)
+
+        if( height[lp] > height[rp] )
+            rp--
+        else
+            lp++
+        
+        res = Math.max(res, area);
+    }
+    
+    return res;
+};
+
+/*
+1*1 = 1
+2*1 = 1
+
+5*8 40
+7*7  49
+
+
+7*1 = 7
+
+
+*/
