ea409e38ed092e4123071a50729bd8ac712daff4
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-26 23:03:40 +0530

    Time: 116 ms (48.29%), Space: 45.2 MB (52.12%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -3,31 +3,49 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    let eval = {
-        "+": (a, b) => {
-            return a + b;
+    let map = {
+        "+" : function(a, b) {
+            return a+b;
         },
-        "-": (a, b) => {
-            return a - b;
+        "-" : function(a, b) {
+            return a-b;
         },
-        "*": (a, b) => {
-            return a * b;
+        "*" : function(a, b) {
+            return a*b;
         },
-        "/": (a, b) => {
-            return ~~(a/b);
+        "/" : function(a, b) {
+            return Math.trunc(a/b);
         }
     }
     let stack = [];
     
     for( let token of tokens ) {
-        if( eval[token] ) {
+        if( map.hasOwnProperty(token) ) {
             let b = stack.pop();
             let a = stack.pop();
-            stack.push(eval[token](a, b))
+            
+            stack.push(map[token](a, b));
         } else {
-            stack.push(parseInt(token));
+            stack.push(+token);
         }
     }
     
     return stack[0];
 };
+
+/*
+["2","1","+","3","*"]
+
+9
+
+["4","13","5","/","+"]
+
+
+2
+4
+
+["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+
+22
+
+*/
