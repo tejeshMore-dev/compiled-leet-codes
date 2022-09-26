93738fe630230767cf38362e335bced6bbd265b9
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-26 21:35:16 +0530

    Time: 78 ms (81.69%), Space: 42.8 MB (34.43%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,23 +3,32 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    const bracketMap = {
+    let stack = [];
+    let mapOfBrackets =  {
+        "}": "{",
         ")": "(",
         "]": "[",
-        "}": "{",
-    };
-    let stack = [];
-    
-    for( let char of s ) {
-        if( !bracketMap.hasOwnProperty(char) ) {
-            stack.push(char);
-        } else {
-            if( !stack.length || stack[stack.length-1] !== bracketMap[char])
-                return false;
-            
-            stack.pop();
-        }
     }
     
-    return !stack.length;
+    for( let char of s ) {
+        if( mapOfBrackets.hasOwnProperty(char) ) {
+            if( ! (stack.length && mapOfBrackets[char] === stack[stack.length-1]) )
+                return false;
+            else
+                stack.pop();
+        } else 
+            stack.push(char);
+    }
+    
+    return stack.length === 0
 };
+
+/*
+ |
+"([]())"
+
+50
+
+1 - 50
+[]
+*/
