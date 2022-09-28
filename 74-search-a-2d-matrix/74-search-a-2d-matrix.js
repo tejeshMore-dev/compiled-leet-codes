094985199140b4c9a36ebf594ba25781d9756d74
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-09-28 11:41:39 +0530

    Time: 105 ms (27.71%), Space: 41.9 MB (79.37%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -3,22 +3,52 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
+var searchMatrix = function(matrix, target) {    
     let rows = matrix.length, cols = matrix[0].length;
-    let lp = 0, rp = rows * cols-1;
-    let result = false;
+    let lp = 0, rp =  rows * cols -1;
+    let res = false;
     
-    while( lp <= rp ) {
-        let mid = Math.floor( lp + (rp-lp)/2 );
-        let midVal = matrix[ Math.floor(mid/cols) ][mid%cols];
+    while( lp < rp ) {
+        let mid = lp + Math.ceil((rp-lp)/2);
+        let row = Math.floor(mid/cols);
+        let col = mid%cols;
+        let val = matrix[row][col];
         
-        if( midVal === target )
-            return result = true;
-        else if( midVal < target )
-            lp = mid+1;
-        else
+        if( val > target )
             rp = mid-1;
+        else
+            lp = mid;
     }
     
-    return result;
+    return matrix[Math.floor(lp/cols)][rp%cols] === target;
 };
+
+/*
+
+
+
+1,3,5,7,10,11,16,20.23,30,34,60
+l
+                              r
+                              
+3
+0 3
+
+6
+r c
+1 2
+
+4
+1 0
+
+7
+r c
+1  3
+
+
+
+
+
+
+
+*/
