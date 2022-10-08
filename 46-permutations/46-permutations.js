d8507b0f6841f3ac4e9eec50c48dd05e31405f2d
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-08 22:38:11 +0530

    Time: 149 ms (8.58%), Space: 45.1 MB (43.43%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -3,24 +3,47 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    let ans = [], temp  = [], used = new Set();
-    findPermutation( temp, used );
-    return ans;
+    let res = [];
+    findPermute(0, new Array(nums.length).fill("-"));
+    return res;
     
-    function findPermutation( temp, used ) {
-        if( temp.length === nums.length )
-            ans.push([ ...temp ]);
+    function findPermute(index, current) {
+        if(index === nums.length) {
+            res.push(current);
+            return;
+        }
         
-        for( let i = 0; i < nums.length; i++ ) {
-            if( used.has(nums[i]) )
-                continue;
+        
+        for( let i=0; i<current.length; i++ ) {
+            let char = current[i];
             
-            used.add(nums[i]);
-            temp.push(nums[i]);
-            findPermutation( temp, used );
-            
-            temp.pop();
-            used.delete(nums[i]);
+            if( char === "-" ) {
+                let comb = [ ...current ]
+                comb[i] = nums[index];
+                findPermute(index+1, comb)
+            }
         }
     }
 };
+/*
+[- - -]
+
+0
+1-- 
+-1- 
+--1 
+
+12-
+1-2
+21-
+-12
+2-1
+-21
+
+123
+132
+213
+312
+231
+321
+*/
