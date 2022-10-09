8650da303b8f7b6d6cfc88f0606fc59fa9f7f588
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-09 22:10:37 +0530

    Time: 1887 ms (28.23%), Space: 48.3 MB (42.94%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -3,40 +3,74 @@
  * @param {string} word
  * @return {boolean}
  */
-
-
 var exist = function(board, word) {
-    let rows = board.length, cols = board[0].length;
+    let rows = board.length;
+    let cols = board[0].length;
+    let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
     
-    for( let r = 0; r < rows; r++ ) {
-        for( let c = 0 ; c < cols; c++ ) {
-            if( dfs(r, c, 0) )
-                return true
+    for( let r=0; r<rows; r++ ) { 
+        for( let c=0; c<cols; c++ ) {
+            if( board[r][c] === word[0] ) {
+                if( findWord(r, c, 0, new Set()) )
+                    return true
+            }
         }
     }
     
     return false;
     
-    function dfs( r, c, i ) {
-        if( i === word.length )
-            return true;
+    function findWord(r, c, i, used) {
+        if( i === word.length) {
+            return true
+        }
         
-        if( r < 0 || c < 0 || r >= rows || c >= cols || word[i] !== board[r][c] )
+        if( r<0 || r >=rows || c<0 || c>=cols || used.has(`${r}-${c}`) || board[r][c] !== word[i])
             return false;
         
-        let temp = board[r][c];
-        board[r][c] = "*"
+        used.add(`${r}-${c}`);
         
-        let res = dfs( r+1, c, i+1 ) || 
-            dfs( r-1, c, i+1 ) || 
-            dfs( r, c-1, i+1 ) || 
-            dfs( r, c+1, i+1 );
-
-        board[r][c] = temp;
-        return res;        
+        let res = findWord(r+1, c, i+1, used) ||
+        findWord(r-1, c, i+1, used) ||
+        findWord(r, c+1, i+1, used) ||
+        findWord(r, c-1, i+1, used)
+        
+        used.delete(`${r}-${c}`);
+        return res
+        
+//                 continue;
+//         for( let direction of directions ) {
+//             let newR = direction[0]+r, newC = direction[1]+c;
+//             if( newR<0 || newR >=rows || newC<0 || newC>=cols || used.has(`${newR}-${newC}`) || board[newR][newC] !== word[i])
+//                 continue;
+            
+//             used.add(`${newR}-${newC}`);
+//             return findWord(newR, newC, i+1, used )
+//             used.delete(`${newR}-${newC}`);
+//         }
+        
+        
+        
+        // return false
     }
-    
 };
+/*
+
+row
+col
+
+for row
+for col
+    board[row][col] === word[wi]
+        add in used
+        recursion (wi++, used)
 
 
 
+states
+wi
+
+used set
+row-col
+
+[1, 0] [-1 0] [0 1] [0 -1]
+*/
