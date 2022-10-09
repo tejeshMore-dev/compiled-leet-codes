02bdad01348b0f7bafebf0ca2f1a691703f1dbf8
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-09 21:47:25 +0530

    Time: 111 ms (13.28%), Space: 42.1 MB (53.83%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -2,36 +2,51 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) { // TC: O(4^N) SC: O(N)
-    if( digits.length === 0 )
-        return []
+var letterCombinations = function(digits) {
+    if(!digits.length)
+        return [];
     
-    let result = [], temp = [];
-    let map = {
-        "2": "abc",
-        "3": "def",
-        "4": "ghi",
-        "5": "jkl",
-        "6": "mno",
-        "7": "pqrs",
-        "8": "tuv",
-        "9" :"wxyz"
-    }
+    let letterMap = {
+        '2': ["a", "b", "c"],
+        '3': ["d", "e", "f"],
+        '4': ["g", "h", "i"],
+        '5': ["j", "k", "l"],
+        '6': ["m", "n", "o"],
+        '7': ["p", "q", "r", "s"],
+        '8': ["t", "u", "v"],
+        '9': ["w", "x", "y", "z"]
+    };
+    let res = [];
+    findLetterCombinations(0, []);
+    return res;
     
-    findCombination(0);
-    return result;
-    
-    function findCombination( i ) {
+    function findLetterCombinations(i, current) {
         if( i === digits.length ) {
-            result.push( temp.join("") );
+            res.push(current.join(""));
             return;
         }
         
-        for( let char of map[digits[i]] ) {
-            temp.push( char );
-            findCombination(i+1);
-            
-            temp.pop();
+        
+        for( let char of letterMap[digits[i]] ) {
+            findLetterCombinations(i+1, [ ...current, char ] )
         }
     }
 };
+
+/*
+2 : [ a b c ]
+3: [ d e f ]
+
+
+
+9: [w x y z]
+
+"23"
+
+i
+0
+[a] [b] [c]
+
+1
+[ad] [ae] [af]
+*/
