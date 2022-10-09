64e207383a2193a2d096a0e527cdd34ad229974b
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-09 22:22:59 +0530

    Time: 857 ms (51.69%), Space: 42.6 MB (81.50%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -10,47 +10,45 @@ var exist = function(board, word) {
     
     for( let r=0; r<rows; r++ ) { 
         for( let c=0; c<cols; c++ ) {
-            if( board[r][c] === word[0] ) {
-                if( findWord(r, c, 0, new Set()) )
-                    return true
-            }
+            if( findWord(r, c, 0) )
+                return true
         }
     }
     
     return false;
     
-    function findWord(r, c, i, used) {
-        if( i === word.length) {
+    function findWord(r, c, i) {
+        if( i === word.length)
             return true
-        }
         
-        if( r<0 || r >=rows || c<0 || c>=cols || used.has(`${r}-${c}`) || board[r][c] !== word[i])
+        if( r<0 || r >=rows || c<0 || c>=cols || board[r][c] !== word[i])
             return false;
         
-        used.add(`${r}-${c}`);
+        let temp = board[r][c];
+        board[r][c] = "*";
         
-        let res = findWord(r+1, c, i+1, used) ||
-        findWord(r-1, c, i+1, used) ||
-        findWord(r, c+1, i+1, used) ||
-        findWord(r, c-1, i+1, used)
+        let res = findWord(r+1, c, i+1) ||
+        findWord(r-1, c, i+1) ||
+        findWord(r, c+1, i+1) ||
+        findWord(r, c-1, i+1)
         
-        used.delete(`${r}-${c}`);
+        board[r][c] = temp;
         return res
-        
-//                 continue;
-//         for( let direction of directions ) {
-//             let newR = direction[0]+r, newC = direction[1]+c;
-//             if( newR<0 || newR >=rows || newC<0 || newC>=cols || used.has(`${newR}-${newC}`) || board[newR][newC] !== word[i])
-//                 continue;
+    
+        /*
+        let res = false;
+        for( let direction of directions ) {
+            let newR = direction[0]+r, newC = direction[1]+c;
+            if( newR<0 || newR >=rows || newC<0 || newC>=cols || used.has(`${newR}-${newC}`) || board[newR][newC] !== word[i])
+                continue;
             
-//             used.add(`${newR}-${newC}`);
-//             return findWord(newR, newC, i+1, used )
-//             used.delete(`${newR}-${newC}`);
-//         }
+            used.add(`${newR}-${newC}`);
+            res = res || findWord(newR, newC, i+1, used )
+            used.delete(`${newR}-${newC}`);
+        }
         
-        
-        
-        // return false
+        return res
+        */
     }
 };
 /*
