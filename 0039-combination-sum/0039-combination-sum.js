b087ef1dd12eda83c1ab5283c3d4405a4c06670b
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-11 10:29:07 +0530

    Time: 100 ms (85.73%), Space: 45.3 MB (76.64%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let res = [], current = [];
+    findCombinationSum(0, 0);
+    return res;
+    
+    
+    function findCombinationSum(i, sum) {
+        if( i === candidates.length || sum >= target ) {
+            if ( sum === target ) {
+                res.push([ ...current ]);
+            }
+            return;
+        }
+        
+        current.push(candidates[i]);
+        findCombinationSum(i, sum + candidates[i]);
+        // findCombinationSum(i+1, sum + candidates[i]);
+        
+        current.pop();
+        findCombinationSum(i+1, sum);
+    }
+};
+/*
+
+
+
+
+
+
+
+
+
+*/
