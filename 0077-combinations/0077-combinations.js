4f48342d607469d4b040db9dee7ecb6b5a178ddf
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-11 10:38:09 +0530

    Time: 219 ms (35.21%), Space: 48.6 MB (45.38%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -4,9 +4,6 @@
  * @return {number[][]}
  */
 var combine = function(n, k) {
-    // if( n === 1 )
-    //     return [[1]];
-    
     let res = [], current = [];
     findCombine(1, 0);
     return res;
