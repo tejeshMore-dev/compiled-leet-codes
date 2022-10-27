d91f28d43b784ac811381795978fc3c61211e146
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-27 22:56:37 +0530

    Time: 92 ms (62.28%), Space: 42.4 MB (20.99%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    if( nums.length === 0 )
+       return 0
+    
+    if( val > 50 )
+        return nums.length;
+    
+    let i=0, ri=0;
+    
+    while( i<nums.length) {
+        if( nums[i] !== val ) {
+            nums[ri] = nums[i];
+            ri++;
+        }
+        
+        i++;
+    }
+    
+    return ri;
+};
+/*
+nums = [3,2,2,3], val = 3
+
+i  3
+ri 2
+
+if nums[i] !== val
+nums[ri] = nums[i]
+ri++;
+
+i++;
+
+
+return ri
+
+*/
