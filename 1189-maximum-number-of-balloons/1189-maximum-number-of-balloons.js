f5f22e7ba658d19f0ed02984f06969cce9c78b55
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-29 10:26:13 +0530

    Time: 131 ms (16.88%), Space: 45.3 MB (5.97%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    let refMap = {}, inputMap = {};
+    
+    for(let char of "balloon") {
+        if( !refMap[char] )
+            refMap[char] = 0;
+        
+        refMap[char] += 1;
+    }
+    
+    for(let char of text) {
+        if( !inputMap[char] )
+            inputMap[char] = 0;
+        
+        inputMap[char] += 1;
+    }
+    let res = Number.MAX_VALUE;
+    for(let char of "balloon") {
+        if( !inputMap[char] )
+            return 0;
+        
+        let inst = Math.floor(inputMap[char] / refMap[char]);
+        
+        if( !inst)
+            return 0
+        res = Math.min(res, inst)
+    }
+    
+    return res
+};
