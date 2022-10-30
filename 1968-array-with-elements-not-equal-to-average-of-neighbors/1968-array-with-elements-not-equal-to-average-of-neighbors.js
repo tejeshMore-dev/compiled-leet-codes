eaf454cac8f4d9756e9313702746e80efc224a03
Tejesh More <82724069+tejeshMore-dev@users.noreply.github.com>
2022-10-30 14:30:29 +0530

    Time: 727 ms (65.79%), Space: 104.1 MB (7.89%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.js
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.js
@@ -3,17 +3,33 @@
  * @return {number[]}
  */
 var rearrangeArray = function(nums) {
-    nums.sort((a,b) => a-b);
-    let lp=0, rp = nums.length-1;
-    let ans = new Array(nums.length), ri = 0;
-    
-    while( lp < rp) {
-        ans[ri++] = nums[lp++];
-        ans[ri++] = nums[rp--];
-        
-        if( lp === rp )
-            ans[ri] = nums[lp];
+    let ex = false;
+    for(let i=1; i<nums.length-1; ++i) {
+        if(nums[i-1]+nums[i+1]===2*nums[i]) {
+            ex = true;
+            const temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
+        }
     }
-
-    return ans;
+    if(ex) {
+        return rearrangeArray(nums);
+    }
+    return nums;
 };
+
+// var rearrangeArray = function(nums) {
+//     nums.sort((a,b) => a-b);
+//     let lp=0, rp = nums.length-1;
+//     let ans = new Array(nums.length), ri = 0;
+    
+//     while( lp < rp) {
+//         ans[ri++] = nums[lp++];
+//         ans[ri++] = nums[rp--];
+        
+//         if( lp === rp )
+//             ans[ri] = nums[lp];
+//     }
+
+//     return ans;
+// };
